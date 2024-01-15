60918f5ed5250445e361d4b8a882f08ea76c9cdb
Freddy Solarte <freddyjsolarte@gmail.com>
2024-01-15 10:46:14 -0400

    initialition

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,51 @@
 import type { Config } from 'tailwindcss'
 
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+  darkMode:'class',
+  
+    daisyui: {
+       darkTheme:"ganadexThemeDark", 
+      themes: [
+        {
+          ganadexTheme: {
+            "primary": "#AF842D",
+            "secondary": "#22FF1E",
+            "base-100": "#f3f4f6",
+           /*  header table */
+            "base-200": "#E2E8F0",
+            /*  body table */
+            "neutral": "#F8FAFC",
+            "info": "#22d3ee",
+            "success": "#00c400",
+            "warning": "#ff8900",
+            "error": "#d70c41",
+          },
+          ganadexThemeDark: {
+            "primary": "#22FF1E",
+            "secondary": "#AF842D",
+            "base-100": "#0F172A",
+           /*  header table */
+            "base-200": "#334155",
+            /*  body table */
+            "neutral": "#0F172A",
+            "info": "#22d3ee",
+            "success": "#00c400",
+            "warning": "#ff8900",
+            "error": "#d70c41",
+          },
+        },
+      ]
     },
-  },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+    require("tailwind-scrollbar")({ nocompatible: true })
+],
 }
 export default config
