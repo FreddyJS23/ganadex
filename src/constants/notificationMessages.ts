3269837327e4732f59988003e38a1ce033dd3ab9
Freddy Solarte <freddyjsolarte@gmail.com>
2024-01-15 10:48:43 -0400

    add constant

---
+++ src/constants/notificationMessages.ts
@@ -0,0 +1,8 @@
+const message="El próximo item sera el "
+const messageInFeminine="La próxima item sera el "
+
+
+export const NOTIFICATION_BIRTH=message.replace('item','parto')
+export const NOTIFICATION_CHECKUP=messageInFeminine.replace('item','revision')
+export const NOTIFICATION_DRYING=message.replace('item','secado')
+export const NOTIFICATION_MILK_WEIGHING ="Hoy se debe realizar el pesaje mensual de la leche correspondiente a este mes"
