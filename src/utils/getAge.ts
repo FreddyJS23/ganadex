89ea3f11eb2a4b0d8fe8189c4cfa2f061bcddc31
Freddy Solarte <freddyjsolarte@gmail.com>
2024-01-31 21:25:11 -0400

    add util get age

---
+++ src/utils/getAge.ts
@@ -0,0 +1,12 @@
+export const getAge=(fecha:Date)=>{
+    
+    const fechaActual=new Date()
+    const fechaNacimiento=new Date(fecha)
+    const ageYear=fechaActual.getFullYear() - fechaNacimiento.getFullYear()
+    const ageMonth=fechaActual.getMonth() - fechaNacimiento.getMonth()  
+    const ageDays=fechaActual.getDate() - fechaNacimiento.getDate()
+
+   if (ageYear != 0) return `${ageYear} ${ageYear == 1 ? 'año' : 'años'}`
+   else if (ageMonth + 1 != 0) return `${ageMonth} ${ageMonth == 1 ? 'mes' : 'meses'}`;
+   else if (ageDays != 0) return `${ageDays} ${ageDays == 1 ? 'dia' : 'días'}`;
+}
