87e4c58015c7fcb6141a6bc768275a6a69618362
Freddy Solarte <freddyjsolarte@gmail.com>
2024-02-02 00:28:10 -0400

    add collection states casttle for select multiple

---
+++ src/collections/statesCasttleSelect.ts
@@ -0,0 +1,13 @@
+export const stateCasttleSelect = [
+    { value: 1, label: 'Sano' },
+    { value: 2, label: 'Fallecido' },
+    { value: 3, label: 'Gestacion' },
+    { value: 4, label: 'Lactancia' },
+    { value: 5, label: 'Vendido' },
+    { value: 6, label: 'Pendiente de revision' },
+    { value: 7, label: 'Pendiente de servicio' },
+    { value: 8, label: 'Pendiente de secar' },
+    { value: 9, label: 'Pendiente de numeración' },
+    { value: 10, label: 'Pendiente de capar' },
+    { value: 11, label: 'Pendiente de pesaje leche' },
+];
