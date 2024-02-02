77d0cdd771d4dcad259422a9fe37e35d7938d230
Freddy Solarte <freddyjsolarte@gmail.com>
2024-02-02 00:27:53 -0400

    add collection types casttle for selects

---
+++ src/collections/typeCastleSelect.ts
@@ -0,0 +1,7 @@
+export const typeCasttleSelect = [
+    { value: 1, label: 'Becerro' },
+    { value: 2, label: 'Maute' },
+    { value: 3, label: 'Novillo' },
+    { value: 4, label: 'Adulto' },
+    { value: 5, label: 'Res' },
+];
