3ca1a46e54321e3a1e365def12021b5f37877f5d
Freddy Solarte <freddyjsolarte@gmail.com>
2024-02-02 00:26:05 -0400

    add component select

---
+++ src/components/select/index.tsx
@@ -0,0 +1,39 @@
+import { InputProps } from '@/types';
+import {
+    Select as SelectNextUI,
+    SelectItem,
+} from '@nextui-org/select';
+
+type SelectProps = Pick<
+    InputProps,
+    'id' | 'description' | 'label' | 'required'
+> & {
+    items: { value: string | number; label: string }[];
+};
+
+export const Select = ({
+    id,
+    label,
+    description,
+    required,
+    items,
+}: SelectProps) => {
+    return (
+        <SelectNextUI
+            id={id}
+            label={label}
+            placeholder='d'
+            items={items}
+            variant="underlined"
+            color="primary"
+            description={description}
+            isRequired={required}
+            classNames={{
+                label: 'text-current font-bold',
+                value: 'text-current',
+            }}
+        >
+            {({ label, value }) => <SelectItem key={value}>{label}</SelectItem>}
+        </SelectNextUI>
+    );
+};
