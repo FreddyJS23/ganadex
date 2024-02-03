94dbd2911023a2f200b8fe9a02ab4a6ea510f5ec
Freddy Solarte <freddyjsolarte@gmail.com>
2024-02-03 14:47:12 -0400

    add page form checkUP

---
+++ src/app/(Tables & Forms)/(Tables)/ganado/[id]/revision/registrar/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { formCheckUp } from '@/collections/formsInputs';
+import { Input } from '@/components/Inputs';
+import { Textarea } from '@/components/Textarea';
+import { Button } from '@/ui/Button';
+import { TitlePage } from '@/ui/TitlePage';
+
+export default function Page() {
+    return (
+        <>
+            <TitlePage title="Registrar revision" />
+
+            <form
+                action=""
+                className="flex flex-col items-center gap-6 p-4 max-w-lg m-auto"
+            >
+                <div className="flex gap-6 md:gap-12">
+                    {formCheckUp.map(({ id, label, required }) =>
+                        id == 'tratamiento' ? (
+                            <Textarea
+                                key={id}
+                                id={id}
+                                label={label}
+                                required={required}
+                            />
+                        ) : (
+                            <Input
+                                key={id}
+                                id={id}
+                                label={label}
+                                required={required}
+                                type="text"
+                            />
+                        ),
+                    )}
+                </div>
+                <div className="w-full sm:max-w-72">
+                    <Button content="Registrar" />
+                </div>
+            </form>
+        </>
+    );
+}
