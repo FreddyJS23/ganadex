6a60d8db7f869ac415118829cf110dfa60c38c90
Freddy Solarte <freddyjsolarte@gmail.com>
2024-02-04 18:15:58 -0400

    add page form supplies

---
+++ src/app/(Tables & Forms)/(Tables)/insumos/registrar/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { formSupply } from '@/collections/formsInputs';
+import { Input } from '@/components/Inputs';
+import { Button } from '@/ui/Button';
+import { TitlePage } from '@/ui/TitlePage';
+
+export default function Page() {
+    return (
+        <>
+            <TitlePage title="Registrar Insumo" />
+
+            <form
+                action=""
+                className="flex flex-col items-center gap-6 p-4 max-w-lg m-auto"
+            >
+                <div className="flex flex-col gap-6 md:gap-12 sm:flex-row ">
+                    {formSupply.map(
+                        ({ id, label, required, type, endContent }) => (
+                            <Input
+                                key={id}
+                                id={id}
+                                label={label}
+                                required={required}
+                                type={type}
+                                endContent={endContent}
+                            />
+                        ),
+                    )}
+                </div>
+                <div className="w-full sm:max-w-72">
+                    <Button content="Registrar" />
+                </div>
+            </form>
+        </>
+    );
+}
