4799251585e26d0ccdf792260c8363efc1e733c3
Freddy Solarte <freddyjsolarte@gmail.com>
2024-02-05 10:41:27 -0400

    add page form staff

---
+++ src/app/(Tables & Forms)/(Tables)/personal/registrar/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { formStaff } from '@/collections/formsInputs';
+import { Input } from '@/components/Inputs';
+import { Button } from '@/ui/Button';
+import { TitlePage } from '@/ui/TitlePage';
+
+export default function Page() {
+    return (
+        <>
+            <TitlePage title="Registrar personal" />
+
+            <form
+                action=""
+                className="flex flex-col items-center gap-6 p-4 m-auto max-w-[827px]"
+            >
+                <div className="flex flex-col gap-6 flex-wrap justify-around md:gap-12 sm:flex-row ">
+                    {formStaff.map(
+                        ({ id, label, required, type, endContent }) => (
+                            <div key={id} className="sm:w-44">
+                                <Input
+                                    key={id}
+                                    id={id}
+                                    label={label}
+                                    required={required}
+                                    type={type}
+                                    endContent={endContent}
+                                />
+                            </div>
+                        ),
+                    )}
+                </div>
+                <div className="w-full sm:max-w-72">
+                    <Button content="Registrar" />
+                </div>
+            </form>
+        </>
+    );
+}
