12be03c2d2befd6e306949f6483e58255e7dbbd5
Freddy Solarte <freddyjsolarte@gmail.com>
2024-02-25 04:40:59 -0400

    add page for get all supplies

---
+++ src/app/insumos/page.tsx
@@ -0,0 +1,13 @@
+import { TableSupplies } from "@/components/tables/supplies"
+import { ResponseInsumos } from "@/types";
+import { TitlePage } from "@/ui/TitlePage";
+import { getData } from "@/utils/getData";
+
+export default async function Page() {
+  const { insumos }: ResponseInsumos = await getData('response_insumos');
+  return (
+    <section>
+      <TitlePage title="Insumos" />
+      <TableSupplies insumos={insumos} /></section>
+  )
+}
