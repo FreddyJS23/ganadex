b2cb8468e4b2a9ace83d8c8c6ccbd1578d35e7ec
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-05 23:30:02 -0400

    create table all services

---
+++ src/components/tables/serve/index.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import {  headerAllServes } from '@/collections/headerColums';
+import {  ResponseServiciosGeneral, Servicios, ToroDeServicio } from '@/types';
+import {
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell,
+} from '@nextui-org/table';
+import { useCallback } from 'react';
+import { LayoutTable } from '..';
+import Link from 'next/link';
+
+export const TableAllServes = ({ todos_servicios }: ResponseServiciosGeneral) => {
+    const renderCell = useCallback(
+        (servicios: Servicios, columnKey: keyof Servicios): any => {
+            const cellValue = servicios[columnKey];
+            switch (columnKey) {
+                case 'toro':
+                      const toro = cellValue as ToroDeServicio;
+                     
+                      return (
+                          <Link href={`toro/${toro.id}`}>
+                              {toro.numero}
+                          </Link>
+                      );
+                    break;
+            
+                default:
+                    break;
+            }
+
+            return cellValue;
+        },
+        [],
+    );
+
+    return (
+        <LayoutTable type="serves">
+            <TableHeader columns={headerAllServes}>
+                {({ key, label }) => (
+                    <TableColumn key={key}>{label}</TableColumn>
+                )}
+            </TableHeader>
+            <TableBody items={todos_servicios}>
+                {(servicios) => (
+                    <TableRow key={servicios.id}>
+                        {(columnKey: any) => (
+                            <TableCell>
+                                {renderCell(servicios, columnKey)}
+                            </TableCell>
+                        )}
+                    </TableRow>
+                )}
+            </TableBody>
+        </LayoutTable>
+    );
+};
