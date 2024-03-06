e22285409b3b0d6b9e313a2494ef80d6f3f54a4d
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-05 23:30:11 -0400

    create page get all services

---
+++ src/app/servicios/page.tsx
@@ -0,0 +1,15 @@
+import { TableAllServes } from "@/components/tables/serve";
+import {  ResponseServiciosGeneral } from "@/types";
+import { TitlePage } from "@/ui/TitlePage";
+import { getData } from "@/utils/getData";
+
+export default async function Page() {
+  const { todos_servicios}: ResponseServiciosGeneral = await getData('response_serviciosGeneral');
+
+  return (
+    <section>
+      <TitlePage title="Servicios generales" />
+      <TableAllServes todos_servicios={todos_servicios} /> 
+      </section>
+  )
+}
