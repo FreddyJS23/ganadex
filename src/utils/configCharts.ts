2a1f6cc1490004f77288d921cc61e4aac8dfd4b2
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-06 22:09:07 -0400

    feat: add opacity background color charts

--- src/utils/configCharts.ts
+++ src/utils/configCharts.ts
@@ -1,13 +1,13 @@
 import { ChartOptions } from 'chart.js';
 
 export const paletteBackground = [
-    '#1FFF00',
-    '#ff4816',
-    '#2dde98',
-    '#2529d8',
-    '#fff000',
-    '#f2b600',
-    '#ffa600',
+    '#1FFF0090',
+    '#ff481690',
+    '#2dde9890',
+    '#2529d890',
+    '#fff00090',
+    '#f2b60090',
+    '#ffa60090',
 ];
 
 export const optionChartTotalTypesCattle: ChartOptions<'doughnut'> = {
