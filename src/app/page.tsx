21780da9403d05b7c3c069fff6f65b657efb0a8d
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-06 21:56:57 -0400

    add chart total types cattle to dashboard
    tambien se agregaron algunos cambios de estilos adicionales t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 import { CardDashboard } from '@/components/cards';
+import { TortaTipoGanado } from '@/components/charts/dashboard';
+import { ResponseTotalTiposGanado } from '@/types/dashboard';
+import { getData } from '@/utils/getData';
+import IconCatle from '@/icons/icono-ganado.svg'
 
-export default function Home() {
-    return (
+export default async function Home() {
+    
+    const {total_tipos_ganado}:ResponseTotalTiposGanado =await getData('response_totalGanadoPorTipo');
+    
+     return (
         <section className="flex flex-col gap-8 justify-center items-center max-w-5xl m-auto sm:grid grid-cols-4 sm:gap-4 sm:gap-y-12 sm:p-4 sm:pl-8 md:items-center xl:pl-0">
             {/*  grafico torta */}
-            <article className="p-2 col-span-full max-w-xl md:col-span-2">
-                <div className="w-full h-52 shadow-cards">
-                    torta ganado
+            <article className="p-4 bg-base-100 col-span-full max-w-xl md:col-span-2 lex justify-center flex-col  w-full shadow-cards">
+                <div className="flex justify-between">
+                    <h3>Cabezas de ganado</h3>
+                    <IconCatle
+                        className={'size-8 bg-primary opacity-70 p-1 rounded-full'}
+                    />
                 </div>
+
+                <TortaTipoGanado total_tipos_ganado={total_tipos_ganado} />
             </article>
             {/* cards  */}
             <article className="col-span-full flex flex-wrap gap-8 w-full p-4  justify-center  sm:justify-around md:col-start-3 md:grid md:grid-cols-2 md:p-0 md:gap-4 md:justify-normal ">
