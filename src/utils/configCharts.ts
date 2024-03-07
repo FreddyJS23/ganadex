09108cb2ffb6d238c6eeede59359cf6f45deaa22
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-07 11:37:07 -0400

    add palette color border chart

--- src/utils/configCharts.ts
+++ src/utils/configCharts.ts
@@ -10,6 +10,16 @@ export const paletteBackground = [
     '#ffa60090',
 ];
 
+export const paletteBorderColor = [
+    '#1FFF00',
+    '#ff4816',
+    '#2dde98',
+    '#2529d8',
+    '#fff000',
+    '#f2b600',
+    '#ffa600',
+];
+
 export const optionChartTotalTypesCattle: ChartOptions<'doughnut'> = {
     plugins: {
         legend: {
