541a690dbfa3eebce01eab5590513ff46850c1f7
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-07 23:06:33 -0400

    create chart bar sales annual castle

---
+++ src/components/charts/dashboard/sales catle/index.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { vacaProductora } from '@/types/dashboard';
+import { optionChartLineSalesCatle, optionChartTop3CatleProduction, paletteBackground, paletteBorderColor } from '@/utils/configCharts';
+import { getTopProductionCastle } from '@/utils/convertResponseTopProductionCatles';
+import { ChartData } from 'chart.js';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+);
+
+const meses = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+];
+
+export const SalesCatle = () => {
+    
+
+    const data: ChartData<'bar', number[]> = {
+        labels: meses,
+        datasets: [
+            {
+                data: [2,5],
+                backgroundColor: paletteBackground,
+                borderColor:paletteBorderColor,
+                borderWidth:1
+            },
+            
+        ],
+    };
+
+    return <Bar options={optionChartLineSalesCatle} data={data} />;
+};
