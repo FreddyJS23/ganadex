75fac3507030ec190412e078702589795d9742ce
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-11 12:22:25 -0400

    create page castrate bull calf

---
+++ src/app/(becerros)/capar_becerro/page.tsx
@@ -0,0 +1,17 @@
+import { TableCastreteBullCalf } from '@/components/tables/castrete bull calf ';
+import { ResponseCriasPendienteCapar} from '@/types';
+import { TitlePage } from '@/ui/TitlePage';
+import { getData } from '@/utils/getData';
+
+export default async function Page() {
+    const { crias_pendiente_capar }: ResponseCriasPendienteCapar = await getData(
+        'response_criasPendienteCapar',
+    );
+
+    return (
+        <section>
+            <TitlePage title="Becerros pendiente de capar" />
+            <TableCastreteBullCalf crias_pendiente_capar={crias_pendiente_capar}/>
+        </section>
+    );
+}
