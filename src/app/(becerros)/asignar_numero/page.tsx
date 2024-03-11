3397b799b8191d50050b48e19ab43dcda5957995
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-11 11:13:24 -0400

    create page assignment number calf

---
+++ src/app/(becerros)/asignar_numero/page.tsx
@@ -0,0 +1,18 @@
+import { TableAssignmentNumberBullCalf } from '@/components/tables/assignment number bull calf';
+import { ResponseCriasPendienteNumeracion } from '@/types';
+import { TitlePage } from '@/ui/TitlePage';
+import { getData } from '@/utils/getData';
+
+export default async function Page() {
+    const { crias_pendiente_numeracion }: ResponseCriasPendienteNumeracion =
+        await getData('response_criasPendienteNumeracion');
+
+    return (
+        <section>
+            <TitlePage title="Asignación de números a becerros" />
+            <TableAssignmentNumberBullCalf
+                crias_pendiente_numeracion={crias_pendiente_numeracion}
+            />
+        </section>
+    );
+}
