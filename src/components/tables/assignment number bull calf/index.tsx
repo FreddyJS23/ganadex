a00056d602e814a6a19af7946aab46b1cd553baf
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-11 11:12:08 -0400

    create table assignment number bull calf

---
+++ src/components/tables/assignment number bull calf/index.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { headerAssignmentNumberBullCalf } from '@/collections/headerColums';
+import {
+    CriaPendienteNumeracion,
+    ResponseCriasPendienteNumeracion,
+} from '@/types';
+import {
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell,
+} from '@nextui-org/table';
+import { useCallback } from 'react';
+import { LayoutTable } from '..';
+import IconButton from '@/icons/icono-capar-numeracion.svg';
+import Link from 'next/link';
+
+export const TableAssignmentNumberBullCalf = ({
+    crias_pendiente_numeracion,
+}: ResponseCriasPendienteNumeracion) => {
+    const renderCell = useCallback(
+        (
+            criaPendienteNumeracion: CriaPendienteNumeracion,
+            columnKey: keyof CriaPendienteNumeracion,
+        ): any => {
+            const cellValue = criaPendienteNumeracion[columnKey];
+
+            switch (columnKey) {
+                case 'nombre':
+                    const nombre = cellValue as string;
+                    return (
+                        <Link href={`ganado/${criaPendienteNumeracion['id']}`}>
+                            {nombre}
+                        </Link>
+                    );
+                /* button icon */
+                case 'id':
+                    return (
+                        <Link
+                            href={`asignar_numero/${criaPendienteNumeracion['id']}`}
+                        >
+                            <IconButton className={'size-6'} />
+                        </Link>
+                    );
+
+                default:
+                    return cellValue;
+            }
+        },
+        [],
+    );
+
+    return (
+        <LayoutTable type="assignment bull calf">
+            <TableHeader columns={headerAssignmentNumberBullCalf}>
+                {({ key, label }) => (
+                    <TableColumn key={key}>{label}</TableColumn>
+                )}
+            </TableHeader>
+            <TableBody items={crias_pendiente_numeracion}>
+                {(cria) => (
+                    <TableRow key={cria.id}>
+                        {(columnKey: any) => (
+                            <TableCell>{renderCell(cria, columnKey)}</TableCell>
+                        )}
+                    </TableRow>
+                )}
+            </TableBody>
+        </LayoutTable>
+    );
+};
