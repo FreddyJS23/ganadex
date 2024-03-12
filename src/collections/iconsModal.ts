03535d383b3a4ca3db5e703d9e3f70850e7ccfc2
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-11 22:08:17 -0400

    add collection icons for modals

---
+++ src/collections/iconsModal.ts
@@ -0,0 +1,13 @@
+import dynamic from 'next/dynamic';
+
+export const iconsModal = {
+    weight: dynamic(() => import('@/icons/icono-peso.svg')),
+    bullCalf: dynamic(() => import('@/icons/icono-capar-numeracion.svg')),
+    price: dynamic(() => import('@/icons/icono-precio.svg')),
+    milk: dynamic(() => import('@/icons/icono-leche.svg')),
+    cattleV2: dynamic(() => import('@/icons/icono-ganado1.svg')),
+    dead: dynamic(() => import('@/icons/icono-muerte ganado.svg')),
+    checkUp: dynamic(() => import('@/icons/icono-revision.svg')),
+    pregnancy: dynamic(() => import('@/icons/icono-preñadas.svg')),
+    serve: dynamic(() => import('@/icons/icono-servir.svg')),
+};
