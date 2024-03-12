1fc7ea4ebd0275aa9d0f934728c525762565f3cf
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-12 16:45:54 -0400

    feat: add end content style select input

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -3,10 +3,21 @@ import { Select as SelectNextUI, SelectItem } from '@nextui-org/select';
 
 type SelectProps = Pick<
     InputProps,
-    'id' | 'description' | 'label' | 'required'
+    'id' | 'description' | 'label' | 'required' | 'endContent'
 > & {
     items: { value: string | number; label: string }[];
 };
+
+const EndElement = ({ content }: { content: '$' | 'KG' }) => {
+    return (
+        <div className="pointer-events-none flex items-center">
+            <span className="text-default-400 text-small sm:text-base">
+                {content}
+            </span>
+        </div>
+    );
+};
+
 
 export const Select = ({
     id,
@@ -14,7 +25,14 @@ export const Select = ({
     description,
     required,
     items,
+    endContent
 }: SelectProps) => {
+    const endContents = {
+        dolar: <EndElement content="$" />,
+        weight: <EndElement content="KG" />,
+        'weight-milk': <EndElement content="KG" />,
+    };
+   
     return (
         <SelectNextUI
             id={id}
@@ -29,6 +47,7 @@ export const Select = ({
                 value: 'text-current',
                 popoverContent: 'bg-base-100',
             }}
+            endContent={endContent && endContents[endContent]}
         >
             {({ label, value }) => <SelectItem key={value}>{label}</SelectItem>}
         </SelectNextUI>
