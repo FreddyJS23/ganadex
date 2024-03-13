f56941d15361caf266af5a22730b726936639656
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-12 21:05:14 -0400

    create modal for create customer

---
+++ src/components/modals/create customer/index.tsx
@@ -0,0 +1,33 @@
+import { Input } from '@/components/Inputs';
+import { LayoutModal } from '..';
+import { ModalProps } from '@/types';
+
+export const ModalCreateCustomer = ({
+    isOpen,
+    onClose,
+    onOpen,
+    onOpenChange,
+    dataHeader,
+}: ModalProps) => {
+    return (
+        <LayoutModal
+            icon="customer"
+            titleModal={'Crear nuevo comprador'}
+            footer={true}
+            isOpen={isOpen}
+            onClose={onClose}
+            onOpen={onOpen}
+            onOpenChange={onOpenChange}
+        >
+            <form action="" method="post" className="m-auto w-2/4 ">
+                <Input
+                    id="customer"
+                    label="Comprador"
+                    required
+                    type="text"
+                    size="lg"
+                />
+            </form>
+        </LayoutModal>
+    );
+};
