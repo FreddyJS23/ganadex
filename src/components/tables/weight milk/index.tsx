b827bd8eb85761570b0e9df3db9909027fb85813
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-13 15:34:35 -0400

    add modal to table all weight milk

--- src/components/tables/weight milk/index.tsx
+++ src/components/tables/weight milk/index.tsx
@@ -9,16 +9,26 @@ import {
     TableRow,
     TableCell,
 } from '@nextui-org/table';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { LayoutTable } from '..';
 import IconButton from '@/icons/icono-peso.svg';
 import Link from 'next/link';
 import IconCheck from '@/icons/icono-check.svg';
 import IconError from '@/icons/icono-error.svg';
+import { Button, useDisclosure } from '@nextui-org/react';
+import { ModalWeightMilk } from '@/components/modals/weight milk';
 
 export const TableAllWeightMilk = ({
     todos_pesaje_leche,
 }: ResponsePesajesLecheGeneral) => {
+    const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+    const [dataModal, setDataModal] = useState<PesajesLeche>();
+
+    const openModal = (pesajeLeche: PesajesLeche) => {
+        setDataModal(pesajeLeche);
+        onOpen();
+    };
+
     const renderCell = useCallback(
         (pesajeLeche: PesajesLeche, columnKey: keyof PesajesLeche): any => {
             const cellValue = pesajeLeche[columnKey];
@@ -26,11 +36,14 @@ export const TableAllWeightMilk = ({
             switch (columnKey) {
                 /* button icon */
                 case 'id':
-                    const numero = pesajeLeche['numero'];
+                    setDataModal(pesajeLeche);
                     return (
-                        <Link href={`ganado/${numero}/pesaje_leche/`}>
-                            <IconButton className={'size-6'} />
-                        </Link>
+                        <>
+                            <IconButton
+                                onClick={() => openModal(pesajeLeche)}
+                                className={'size-6 cursor-pointer'}
+                            />
+                        </>
                     );
 
                 case 'pesaje_este_mes':
@@ -49,23 +62,32 @@ export const TableAllWeightMilk = ({
     );
 
     return (
-        <LayoutTable type="all weight milk">
-            <TableHeader columns={headerAllWeightMilk}>
-                {({ key, label }) => (
-                    <TableColumn key={key}>{label}</TableColumn>
-                )}
-            </TableHeader>
-            <TableBody items={todos_pesaje_leche}>
-                {(pesajeLeche) => (
-                    <TableRow key={pesajeLeche.id}>
-                        {(columnKey: any) => (
-                            <TableCell>
-                                {renderCell(pesajeLeche, columnKey)}
-                            </TableCell>
-                        )}
-                    </TableRow>
-                )}
-            </TableBody>
-        </LayoutTable>
+        <>
+            <LayoutTable type="all weight milk">
+                <TableHeader columns={headerAllWeightMilk}>
+                    {({ key, label }) => (
+                        <TableColumn key={key}>{label}</TableColumn>
+                    )}
+                </TableHeader>
+                <TableBody items={todos_pesaje_leche}>
+                    {(pesajeLeche) => (
+                        <TableRow key={pesajeLeche.id}>
+                            {(columnKey: any) => (
+                                <TableCell>
+                                    {renderCell(pesajeLeche, columnKey)}
+                                </TableCell>
+                            )}
+                        </TableRow>
+                    )}
+                </TableBody>
+            </LayoutTable>
+            <ModalWeightMilk
+                isOpen={isOpen}
+                onOpen={onOpen}
+                onOpenChange={onOpenChange}
+                dataHeader={dataModal?.numero}
+                onClose={onClose}
+            />
+        </>
     );
 };
