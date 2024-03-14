4512cee71447090abd3427912558d18546bba4fb
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-14 00:30:18 -0400

    refactor: delete button container tabs
    ahora se usa directamente el tab

--- src/components/tabsDetatilsCattle/item.tsx
+++ src/components/tabsDetatilsCattle/item.tsx
@@ -8,9 +8,6 @@ export const ContainerContentTab = ({ children }: ContainerContentTabProps) => {
     return (
         <div className="grid grid-cols-2 bg-base-100 shadow-[0px_0px_6px_-3px] shadow-primary rounded-lg gap-4 p-4 sm:grid-cols-3 lg:flex w-full">
             {children}
-            <div className="col-span-full place-self-center">
-                <Button content="Ver historial" />
-            </div>
         </div>
     );
 };
