4dbb0ba434c280b82cffb8df70aebdcb9511854e
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-20 13:02:22 -0400

    perf: add key in elements inside map

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -17,8 +17,7 @@ export const CarouselImagesLogin = () => {
         <Slider {...settings}>
             {imagesSliderLogin.map((image) => {
                 return (
-                    <div>
-                        <Image {...image} className="h-[132px] md:h-[140px]" />
+                    <div key={image.alt}>
                     </div>
                 );
             })}
@@ -28,9 +27,9 @@ export const CarouselImagesLogin = () => {
 export const CarouselTextLogin = () => {
     return (
         <Slider {...settings}>
-            {textCarouselLogin.map(({ text }) => {
+            {textCarouselLogin.map(({ text },index) => {
                 return (
-                    <div>
+                    <div key={index} >
                         <p className="text-wrap text-center">{text}</p>
                     </div>
                 );
