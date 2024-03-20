fe6f2455ed9dc48119e70829a1afc004f4202d42
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-20 11:44:22 -0400

    refactor: change logic modal to use route parallel
    eliminadas las colleciones fake credas previmiente, ademas se eliminaron toda la logica para los modales y se sustituyeron los botones de apertura de modal con una navegacion hacia rutas paralelas que contendran los modales

--- src/components/tabsDetatilsCattle/index.tsx
+++ src/components/tabsDetatilsCattle/index.tsx
@@ -2,26 +2,12 @@
 
 import { Details } from '../details';
 import { DetailsChecks, DetailsServe, iconsSidebar } from '@/collections';
-import {
-    Parto,
-    PesajeLecheGanado,
-    ResponseGanado,
-    Revision,
-    Revisiones,
-    Servicio,
-    Servicios,
-} from '@/types';
+import { ResponseGanado,} from '@/types';
 import { ContainerContentTab } from './item';
 import { Tabs, Tab } from '@nextui-org/tabs';
 import { TitleTab } from '@/ui/TitleTab';
 import { Button } from '@/ui/Button';
-import { UseDisclosureProps, useDisclosure } from '@nextui-org/react';
-import { useState } from 'react';
-import { ModalHistoryServices } from '../modals/history services';
-import { ModalHistoryCheckUps } from '../modals/history checkups';
-import { ModalHistoryMilk } from '../modals/history prices milk';
-import { ModalHistoryBirths } from '../modals/history births';
-import { ModalHistoryWeightMilk } from '../modals/history weight milk';
+import { useParams, usePathname, useRouter } from 'next/navigation';
 
 type TabsDetailsCattleProps = Omit<ResponseGanado, 'ganado'>;
 
@@ -31,112 +17,12 @@ export const TabDetailsCattle = ({
     total_revisiones,
     total_servicios,
 }: TabsDetailsCattleProps) => {
-    const fakeRevisiones = [
-        {
-            id: 43564,
-            fecha: new Date('2023-11-23'),
-            diagnostico: 'totam-aspernatur-soluta-eius',
-            tratamiento: 'suscipit-eum',
-        },
-        {
-            id: 434,
-            fecha: new Date('2023-11-23'),
-            diagnostico: 'totam-aspernatur-soluta-eius',
-            tratamiento: 'suscipit-eum',
-        },
-    ];
-    const fakeServicios = [
-        {
-            id: 88928,
-            fecha: new Date('2023-03-31'),
-            observacion: 'molestias-aperiam',
-            tipo: 'neque-quaerat-soluta-corrupti-hic',
-            numero_toro: 889,
-        },
-        {
-            id: 885361378928,
-            fecha: new Date('2023-03-31'),
-            observacion: 'molestias-aperiam',
-            tipo: 'neque-quaerat-soluta-corrupti-hic',
-            numero_toro: 889,
-        },
-    ];
-    const fakePesajesLeche:PesajeLecheGanado[] = [
-        {
-            id: 90,
-            pesaje: '54-KG',
-            fecha: new Date('2023-12-04'),
-        },
-        {
-            id: 9080,
-            pesaje: '44-KG',
-            fecha: new Date('2023-12-04'),
-        },
-    ];
-    const fakePartos:Parto[] = [
-        {
-            id: 221008,
-            fecha: new Date('2023-11-27'),
-            observacion: 'placeat-repellate',
-            cria: {
-                id: 1364,
-                nombre: 'excepturi-amet-saepe-consequuntur',
-                numero: null,
-                sexo: 'M',
-                origen: null,
-                peso_nacimiento: '442-KG',
-                fecha_nacimiento: null,
-            },
-            padre_numero: 7624,
-        },
-        {
-            id: 22108,
-            fecha: new Date('2023-11-27'),
-            observacion: 'placeat-repellat-harum-iste',
-            cria: {
-                id: 14464,
-                nombre: 'excepturi-amet-saepe-consequuntur',
-                numero: null,
-                sexo: 'M',
-                origen: null,
-                peso_nacimiento: '442-KG',
-                fecha_nacimiento: null,
-            },
-            padre_numero: 74624,
-        },
-    ];
+   
+ const router = useRouter();
+ const pathname = usePathname();
 
-    const modalRevision = useDisclosure();
-    const modalServicio = useDisclosure();
-    const modalPesajeLeche = useDisclosure();
-    const modalPartos = useDisclosure();
-
-    const [dataModalRevisiones, setDataModalRevisiones] =
-        useState<Revision[]>();
-    const [dataModalServicios, setDataModalServicios] = useState<Servicio[]>();
-    const [dataModalPesasjeLeche, setDataModalPesasjeLeche] = useState<PesajeLecheGanado[]>();
-    const [dataModalPartos, setDataModalPartos] = useState<Parto[]>();
-
-
-    const openModalRevisiones = (revisiones?: Revision[]) => {
-        setDataModalRevisiones(revisiones);
-        modalRevision.onOpen();
-    };
-    const openModalServicios = (servicios?: Servicio[]) => {
-        setDataModalServicios(servicios);
-        modalServicio.onOpen();
-    };
-    const openModalPesajesLeche = (pesajesLeche?: PesajeLecheGanado[]) => {
-        setDataModalPesasjeLeche(pesajesLeche);
-        modalPesajeLeche.onOpen();
-    };
-    const openModalPartos = (partos?: Parto[]) => {
-        setDataModalPartos(partos);
-        modalPartos.onOpen();
-    };
-
-
-    return (
+    
+ return (
         <>
             {/* secciones */}
             <Tabs
@@ -182,10 +68,10 @@ export const TabDetailsCattle = ({
                         <div className="col-span-full place-self-center">
                             <Button
                                 onClick={() =>
-                                    openModalRevisiones(fakeRevisiones)
+                                  router.push(`${pathname}/revision`)
                                 }
                                 content="Ver historial"
-                            />
+                            /> 
                         </div>
                     </ContainerContentTab>
                 </Tab>
@@ -220,12 +106,12 @@ export const TabDetailsCattle = ({
                             content={'70%'}
                         />
                         <div className="col-span-full place-self-center">
-                            <Button
+                          <Button
                                 onClick={() =>
-                                    openModalServicios(fakeServicios)
+                                  router.push(`${pathname}/servicio`)
                                 }
                                 content="Ver historial"
-                            />
+                            /> 
                         </div>
                     </ContainerContentTab>
                 </Tab>
@@ -234,10 +120,12 @@ export const TabDetailsCattle = ({
                     title={<TitleTab title="Partos" icon="pregnant" />}
                 >
                     <div className="col-span-full place-self-center">
-                        <Button
-                            onClick={() => openModalPartos(fakePartos)}
-                            content="Ver historial"
-                        />
+                   <Button
+                                onClick={() =>
+                                  router.push(`${pathname}/parto`)
+                                }
+                                content="Ver historial"
+                            /> 
                     </div>
                 </Tab>
                 <Tab
@@ -245,51 +133,16 @@ export const TabDetailsCattle = ({
                     title={<TitleTab title="Pesajes de leche" icon="milk" />}
                 >
                     <div className="col-span-full place-self-center">
-                        <Button
-                            onClick={() => openModalPesajesLeche(fakePesajesLeche)}
-                            content="Ver historial"
-                        />
+                    <Button
+                                onClick={() =>
+                                  router.push(`${pathname}/pesaje_leche`)
+                                }
+                                content="Ver historial"
+                            /> 
                     </div>
                 </Tab>
             </Tabs>
 
-            {dataModalServicios && (
-                <ModalHistoryServices
-                    isOpen={modalServicio.isOpen}
-                    onOpen={modalServicio.onOpen}
-                    onOpenChange={modalServicio.onOpenChange}
-                    servicios={dataModalServicios}
-                    onClose={modalServicio.onClose}
-                />
-            )}
-
-            {dataModalRevisiones && (
-                <ModalHistoryCheckUps
-                    isOpen={modalRevision.isOpen}
-                    onOpen={modalRevision.onOpen}
-                    onOpenChange={modalRevision.onOpenChange}
-                    revisiones={dataModalRevisiones}
-                    onClose={modalRevision.onClose}
-                />
-            )}
-            {dataModalPesasjeLeche && (
-                <ModalHistoryWeightMilk
-                    isOpen={modalPesajeLeche.isOpen}
-                    onOpen={modalPesajeLeche.onOpen}
-                    onOpenChange={modalPesajeLeche.onOpenChange}
-                    pesajes_leche={dataModalPesasjeLeche}
-                    onClose={modalPesajeLeche.onClose}
-                />
-            )}
-            {dataModalPartos && (
-                <ModalHistoryBirths
-                    isOpen={modalPartos.isOpen}
-                    onOpen={modalPartos.onOpen}
-                    onOpenChange={modalPartos.onOpenChange}
-                    partos={dataModalPartos}
-                    onClose={modalPartos.onClose}
-                />
-            )}
         </>
     );
 };
