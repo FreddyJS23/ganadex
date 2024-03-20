1330514d6af9f61de0d5c5eef6250f8fa369e964
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-20 12:20:14 -0400

    refactor: change logic modal to use route parallel

--- src/components/modals/history weight milk/index.tsx
+++ src/components/modals/history weight milk/index.tsx
@@ -1,12 +1,10 @@
+'use client'
+
 import { LayoutModal } from '..';
 import { ModalProps, PesajeLecheGanado } from '@/types';
 import { TableHistoryWeightMilk } from '@/components/tables/in modals/history weight milk';
 
 export const ModalHistoryWeightMilk = ({
-    isOpen,
-    onClose,
-    onOpen,
-    onOpenChange,
     pesajes_leche,
 }: ModalProps & { pesajes_leche: PesajeLecheGanado[] }) => {
     return (
@@ -14,10 +12,7 @@ export const ModalHistoryWeightMilk = ({
             icon="milk"
             titleModal={'Historial pesajes mensuales de leche'}
             footer={false}
-            isOpen={isOpen}
-            onClose={onClose}
-            onOpen={onOpen}
-            onOpenChange={onOpenChange}
+            isOpen={true}
         >
             <div className="m-auto mb-4">
                 <TableHistoryWeightMilk pesajes_leche={pesajes_leche} />
