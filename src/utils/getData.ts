b72ceca635d4df7fdb1fea3ea805b3596ad119db
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-08 16:42:00 -0400

    refactor: change logic fethc data

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,15 +1,22 @@
-import { EndPointsFakeApi } from '@/types';
+import { endPoints, endPointsCattle } from '@/collections/endPointsApi';
 
 export async function getData(
-    endPoint: keyof typeof EndPointsFakeApi,
+    endPoint: keyof typeof endPoints,
     id?: number,
+    endPointCattle?: keyof typeof endPointsCattle,
+    id2?: number,
 ) {
-    const res = await fetch(
-        `${process.env.API_URL}${endPoint}${id ? `/${id}` : ''}`,
-        { cache: 'no-store' },
-    );
-    if (!res.ok) {
-        throw new Error('Failed to fetch data');
+    let url = process.env.API_URL + endPoints[endPoint];
+    const optionFetch: RequestInit = { cache: 'no-store' };
+
+    if (id) url = url + id;
+    if (endPointCattle) url = url + endPointsCattle[endPointCattle];
+    if (id2) url = url + id2;
+
+    try {
+        const res = await fetch(url, optionFetch);
+        return res.json();
+    } catch (e) {
+        throw new Error('error in fetch');
     }
-    return res.json();
 }
