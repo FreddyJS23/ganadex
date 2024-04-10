1a60e8b9476d7c4b745120d69cb71437497f643a
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-10 19:15:44 -0400

    refactor: use enum typeNotifiacition to notifications messages

--- src/collections/notifications.ts
+++ src/collections/notifications.ts
@@ -3,6 +3,12 @@ import {
     NOTIFICATION_CHECKUP,
     NOTIFICATION_DRYING,
 } from '../constants';
+import { TypeNotification } from '@/types';
 
 
+
+export const notifications: { [Property in keyof typeof TypeNotification]: string } = {
+    parto: NOTIFICATION_BIRTH,
+    revision: NOTIFICATION_CHECKUP,
+    secado: NOTIFICATION_DRYING,
 };
