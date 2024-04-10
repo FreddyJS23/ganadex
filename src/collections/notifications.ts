bb87d2b3c6eaa6ac29bae254a14530292b829c75
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-10 19:13:15 -0400

    refactor: delete types notifications messages

--- src/collections/notifications.ts
+++ src/collections/notifications.ts
@@ -5,17 +5,5 @@ import {
     NOTIFICATION_MILK_WEIGHING,
 } from '../constants';
 
-/**Mensaje de la notificacion */
-type NotificationMessages = {
-    birth: string;
-    checkup: string;
-    drying: string;
-    milkWeighing: string;
-};
 
-export const notifications: NotificationMessages = {
-    birth: NOTIFICATION_BIRTH,
-    checkup: NOTIFICATION_CHECKUP,
-    drying: NOTIFICATION_DRYING,
-    milkWeighing: NOTIFICATION_MILK_WEIGHING,
 };
