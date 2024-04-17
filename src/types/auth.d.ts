844d21e3e3c8e6d357317390141a7b952f41724e
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-17 12:21:42 -0400

    add types to session & jwt of authjs

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -1,4 +1,8 @@
 type login = {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JWT } from 'next-auth/jwt';
+
+type User = {
     id: number;
     usuario: string;
     token: string;
@@ -11,3 +15,35 @@ export type ResponseRegistroExitoso = {
 export type ResponseLogin = {
     login: login;
 };
+
+export type ResponseLoginAuthJs ={
+    login:boolean;
+    redirect:'/dashboard' | null;
+    message:string | null;
+
+}
+
+declare module 'next-auth' {
+    export interface User  {
+        /** Define any user-specific variables here to make them available to other code inferences */
+        id: number;
+        usuario: string;
+        token: string;
+        // Any other attributes you need from either your User table columns or additional fields during a session callback
+    }
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    export interface Session {
+        user: User;
+    }
+}
+
+declare module 'next-auth/jwt' {
+    export interface JWT {
+        id: number;
+        usuario: string;
+        token: string;
+    }
+    
+}
