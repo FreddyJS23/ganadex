069980e5457d2bb06798de581a755bd7a28cb28d
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-17 12:31:13 -0400

    create action login to authenticate user

---
+++ src/actions/login.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import { signIn } from '@/auth';
+import { ERROR_SERVER, ERROR_SIGNIN } from '@/constants/responseApiMessage';
+import { ResponseLoginAuthJs } from '@/types';
+import { AuthError } from 'next-auth';
+import { isRedirectError } from 'next/dist/client/components/redirect';
+
+export async function authenticate(
+    formData: FormData,
+): Promise<ResponseLoginAuthJs | undefined> {
+    try {
+        await signIn('credentials', {
+            usuario: formData.get('usuario'),
+            password: formData.get('password'),
+            redirectTo: '/dashboard',
+        });
+    } catch (error) {
+        if (isRedirectError(error)) {
+            return {
+                login: true,
+                message: 'Credenciales correctas',
+                redirect: '/dashboard',
+            };
+        }
+        if (error instanceof AuthError) {
+            const regexMessageErrors = new RegExp(
+                `${ERROR_SERVER}|${ERROR_SIGNIN}`,
+            );
+            const messageError = error.message.match(regexMessageErrors);
+            throw messageError && messageError[0]
+        }
+    }
+}
