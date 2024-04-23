af2fd963dbeec90616865d1a67a17cff17676caa
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-23 12:06:04 -0400

    create action to register supply

---
+++ src/actions/createSupply.ts
@@ -0,0 +1,22 @@
+'use serve';
+
+import { ResponseError, ResponseInsumo } from '@/types';
+import { CreateSupply } from '@/types/forms';
+import { getData } from '@/utils/getData';
+
+export async function createSupply(
+    formData: CreateSupply,
+): Promise<string | ResponseError | undefined> {
+    try {
+        const { insumo }: ResponseInsumo = await getData(
+            'insumo',
+            'POST',
+            formData,
+        );
+        return insumo.insumo;
+       
+    } catch (error) {
+        const { message } = error as Error;
+        throw message;
+    }
+}
