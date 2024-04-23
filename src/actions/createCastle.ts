796266f868c3609637d50202c4b0b002f36309c8
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-22 22:50:32 -0400

    fix: change types inputs collections

---
+++ src/actions/createCastle.ts
@@ -0,0 +1,23 @@
+'use serve';
+
+import { ResponseError, ResponseGanado } from '@/types';
+import { CreateCastle } from '@/types/forms';
+import { getData } from '@/utils/getData';
+
+export async function createCastle(
+    formData: CreateCastle,
+): Promise<string | number | ResponseError | undefined> {
+
+    try {
+        const { ganado }: ResponseGanado = await getData(
+            'ganado',
+            'POST',
+            formData,
+        );
+        if (ganado.numero) return ganado.numero;
+        else if (ganado.nombre) return ganado.nombre; 
+    } catch (error) {
+        const { message } = error as Error;
+        throw message;
+    }
+}
