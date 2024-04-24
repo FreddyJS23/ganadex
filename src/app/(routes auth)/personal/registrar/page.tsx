11d43c88ec7402146a8d7ca8e60170598ab85cec
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-24 16:11:48 -0400

    add form create staff

--- src/app/(routes auth)/personal/registrar/page.tsx
+++ src/app/(routes auth)/personal/registrar/page.tsx
@@ -1,40 +1,16 @@
-'use client';
-
-import { formStaff } from '@/collections/formsInputs';
-import { Input } from '@/components/Inputs';
-import { Button } from '@/ui/Button';
+import { FormCreateStaff } from '@/components/forms/create staff';
+import { ResponseCargosPersonal } from '@/types';
 import { TitlePage } from '@/ui/TitlePage';
+import { getData } from '@/utils/getData';
 
-export default function Page() {
-    const submit = () => {};
+export default async function Page() {
+  const {cargos_personal}:ResponseCargosPersonal=await getData('cargosPersonal')
+
     return (
         <>
             <TitlePage title="Registrar personal" />
 
-            <form
-                action=""
-                className="flex flex-col items-center gap-6 p-4 m-auto max-w-[827px]"
-            >
-                <div className="flex flex-col gap-6 flex-wrap justify-around md:gap-12 sm:flex-row ">
-                    {formStaff.map(
-                        ({ id, label, required, type, endContent }) => (
-                            <div key={id} className="sm:w-44">
-                                <Input
-                                    key={id}
-                                    id={id}
-                                    label={label}
-                                    required={required}
-                                    type={type}
-                                    endContent={endContent}
-                                />
-                            </div>
-                        ),
-                    )}
-                </div>
-                <div className="w-full sm:max-w-72">
-                    <Button onClick={submit} content="Registrar" />
-                </div>
-            </form>
+         <FormCreateStaff cargos_personal={cargos_personal} />
         </>
     );
 }
