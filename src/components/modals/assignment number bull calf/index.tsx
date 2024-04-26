6a336b6c6a91ea45ab5d2fc1aed98daf598635f0
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-25 22:34:55 -0400

    use action assignment number calf

--- src/components/modals/assignment number bull calf/index.tsx
+++ src/components/modals/assignment number bull calf/index.tsx
@@ -4,14 +4,43 @@ import { Input } from '@/components/Inputs';
 import { LayoutModal } from '..';
 import { ModalProps } from '@/types';
 import { useDisclosure } from '@nextui-org/react';
+import { CreateAssigmentNumberBullCalf } from '@/types/forms';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {assignmentNumberBullCalf} from '@/actions/assigmentNumberBullCalf'
+import { toast } from 'sonner';
+import { assignmentNumberBullCalfShema } from '@/validations/assignmentNumberBullCalfShema';
 import { useRef } from 'react';
+import { useParams, useRouter } from 'next/navigation';
 
 export const ModalAssignmentNumberBullCalf = ({
     dataHeader,
 }: Pick<ModalProps, 'dataHeader'>) => {
     const { onOpen, onOpenChange } = useDisclosure();
 
+const {
+    register,
+    formState: { errors },
+    handleSubmit,
+} = useForm<CreateAssigmentNumberBullCalf>({resolver:zodResolver(assignmentNumberBullCalfShema)});
+
+const router = useRouter();
 const formRef=useRef(null)
+const params = useParams<{ id: string}>();
+ 
+const actionAssigmentNumberBullCalf: () => void = handleSubmit(async (data) => {
+        try {
+            await assignmentNumberBullCalf(data, parseInt(params.id));
+            toast.success('Operación exitosa');
+              router.back(); 
+              router.refresh(); 
+        } catch (error) {
+            
+            const message = error as string;
+            return toast.error(message);
+        }
+    });
+
     return (
         <LayoutModal
             icon="bullCalf"
