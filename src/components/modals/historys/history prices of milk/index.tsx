9d9b2098dd4d101e1e2369f8c1f99a88817218c5
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-28 17:17:42 -0400

    create modal to histories of prices milk

---
+++ src/components/modals/historys/history prices of milk/index.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { LayoutModal } from '../..';
+import { ModalProps, PreciosDeLeche } from '@/types';
+import { ContainerTableHistory } from '..';
+import { TableHistoryPriceMilk } from '@/components/tables/in modals/history price milk';
+
+export const ModalHistoryPricesMilk = ({
+    precios,
+}: ModalProps & { precios: PreciosDeLeche[] }) => {
+    return (
+        <LayoutModal
+            icon="price"
+            titleModal={'Historial de precio de la leche'}
+            footer={false}
+            isOpen={true}
+        >
+            <ContainerTableHistory>
+                <TableHistoryPriceMilk precios={precios} />
+            </ContainerTableHistory>
+        </LayoutModal>
+    );
+};
