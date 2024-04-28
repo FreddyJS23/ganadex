4ed0321844ff9e1e569557effa986fdef0237336
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-28 17:37:28 -0400

    create routes register & index prices milk

---
+++ src/app/(routes auth)/precio/page.tsx
@@ -0,0 +1,10 @@
+import { ModalHistoryPricesMilk } from '@/components/modals/historys/history prices of milk';
+import {ResponsePreciosLeche } from '@/types';
+import { getData } from '@/utils/getData';
+
+
+export default async function Page() {
+    const { precios }: ResponsePreciosLeche = await getData('preciosLeche');
+
+    return <ModalHistoryPricesMilk precios={precios} />;
+}
