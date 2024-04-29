0e369da1d2c32099de60284baa473d7af54fad4b
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-28 22:34:42 -0400

    fix: add validation reges to strings

--- src/validations/Customer.ts
+++ src/validations/Customer.ts
@@ -1,5 +1,9 @@
 import { z } from './configInitZod';
 
 export const createCustomerShema = z.object({
-    nombre: z.string().min(3).max(3),
+    nombre: z
+        .string()
+        .regex(/^[a-zA-ZÀ-ÿ\u00f1\u00d1/\sa-zA-ZÀ-ÿ\u00f1\u00d1]{3,30}$/)
+        .min(3)
+        .max(30),
 });
