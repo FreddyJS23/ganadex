2ba325285f6426e348166d357f34617b03a04abd
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-29 22:40:12 -0400

    create action to generate reports

---
+++ src/actions/generate report.ts
@@ -0,0 +1,22 @@
+'use serve';
+
+import { endpointsReports } from '@/collections/endPointsApi';
+import { ResponseError } from '@/types';
+import { RangeDatesToReports } from '@/types/forms';
+
+export async function generateReports(
+    formData: RangeDatesToReports,
+    report: keyof typeof endpointsReports,
+): Promise<Blob | ResponseError | undefined> {
+    try {
+        const file = await fetch(
+            `http://localhost:3000/api/reportes/${report}?start=${formData.start}&end=${formData.end}`,
+        );
+        if (file.status == 200) return await file.blob();
+        else throw (file) ?? 'error';
+    
+    } catch (error) {
+        const { message } = error as Error;
+        throw message;
+    }
+}
