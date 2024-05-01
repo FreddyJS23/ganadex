7fa688c10277801310b42a6298c67bc31b90b175
Freddy Solarte <freddyjsolarte@gmail.com>
2024-05-01 16:19:49 -0400

    change validation string for enum form serve

--- src/validations/serveShema.ts
+++ src/validations/serveShema.ts
@@ -3,6 +3,6 @@ import { z } from './configInitZod';
 export const createServeShema = z.object({
     observacion: z.string().min(3).max(255),
     numero_toro: z.number(),
-    tipo: z.string(),
+    tipo: z.enum(['Monta', 'Inseminacion']),
     personal_id: z.string().regex(/\d/),
 });
