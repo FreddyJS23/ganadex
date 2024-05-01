9a9d247efb7c1bda54239b91f32d3c6fe694f57c
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-30 22:33:05 -0400

    add logic to create weight milk

--- src/components/modals/weight milk/index.tsx
+++ src/components/modals/weight milk/index.tsx
@@ -4,11 +4,46 @@ import { Input } from '@/components/Inputs';
 import { LayoutModal } from '..';
 import { ModalProps } from '@/types';
 import { useDisclosure } from '@nextui-org/react';
+import { useForm } from 'react-hook-form';
+import { CreateWeightMilk } from '@/types/forms';
+import { createWeightMilkShema } from '@/validations/WeightMilkShema';
+import { useParams, useRouter } from 'next/navigation';
+import { useRef } from 'react';
+import { toast } from 'sonner';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createWeightMilk } from '@/actions/create weightMilk';
 
 export const ModalWeightMilk = ({
     dataHeader,
 }: Pick<ModalProps, 'dataHeader'>) => {
     const { onOpen, onOpenChange } = useDisclosure();
+
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm<CreateWeightMilk>({
+        resolver: zodResolver(createWeightMilkShema),
+    });
+
+    const router = useRouter();
+    const formRef = useRef(null);
+    const params = useParams<{ id: string }>();
+
+    const actionCreateWeightMilk: () => void = handleSubmit(async (data) => {
+        try {
+            const weightMilk = await createWeightMilk(
+                data,
+                parseInt(params.id),
+            );
+            toast.success(`Pesaje de ${weightMilk}KG de la vaca ${dataHeader} ha sido registrado`);
+            router.back();
+            router.refresh();
+        } catch (error) {
+            const message = error as string;
+            return toast.error(message);
+        }
+    });
 
     return (
         <LayoutModal
@@ -19,15 +54,23 @@ export const ModalWeightMilk = ({
             isOpen={true}
             onOpen={onOpen}
             onOpenChange={onOpenChange}
+            refForm={formRef}
         >
-            <form action="" method="post" className="m-auto w-2/4 ">
+            <form
+                action={actionCreateWeightMilk}
+                id="form-weightMilkCattle"
+                ref={formRef}
+                className="m-auto w-2/4 "
+            >
                 <Input
-                    id="pesajeLeche"
+                    id="peso_leche"
                     label="Pesaje"
                     required
                     type="number"
                     endContent="weight-milk"
                     size="lg"
+                    register={register}
+                    errors={errors}
                 />
             </form>
         </LayoutModal>
