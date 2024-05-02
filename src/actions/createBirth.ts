48721bb2af99a985ea10e1f95f6aed0979b87a1f
Freddy Solarte <freddyjsolarte@gmail.com>
2024-05-01 22:47:24 -0400

    create action to register birth

---
+++ src/actions/createBirth.ts
@@ -0,0 +1,24 @@
+'use serve';
+
+import { ResponseError,} from '@/types';
+import { CreateBirth, } from '@/types/forms';
+import { getData } from '@/utils/getData';
+
+export async function createBirth(
+    formData: CreateBirth,
+    id: number,
+): Promise<void |ResponseError | undefined>  {
+    try {
+        await getData(
+            'ganado',
+            'POST',
+            formData,
+            id,
+            'parto',
+        );
+
+    } catch (error) {
+        const { message } = error as Error;
+        throw message;
+    }
+}
