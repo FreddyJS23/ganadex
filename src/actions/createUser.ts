636a7edbf67a8996b27ee1ae95ac0db526b91af5
Freddy Solarte <freddyjsolarte@gmail.com>
2024-05-02 17:52:44 -0400

    refactor: use service create user

--- src/actions/createUser.ts
+++ src/actions/createUser.ts
@@ -1,18 +1,14 @@
 'use serve';
 
+import { createUserApi } from '@/services/createUser';
 import { ResponseCorrect, ResponseError } from '@/types';
 import { CreateUser } from '@/types/forms';
-import { getData } from '@/utils/getData';
 
 export async function createUser(
     formData: CreateUser,
 ): Promise<string | ResponseError> {
     try {
-        const { message }: ResponseCorrect = await getData(
-            'crearUsuario',
-            'POST',
-            formData,
-        );
+        const { message }: ResponseCorrect = await createUserApi(formData,);
         return message;
     } catch (error) {
         const { data, status } = error as ResponseError;
