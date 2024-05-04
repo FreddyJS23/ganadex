c9c3e7f992801f2ead343450f234072439c7e32e
Freddy Solarte <freddyjsolarte@gmail.com>
2024-05-04 10:33:33 -0400

    create page to view all bulls

---
+++ src/app/(routes auth)/toros/page.tsx
@@ -0,0 +1,15 @@
+import { TableBull } from '@/components/tables/bull';
+import { ResponseToros } from '@/types';
+import { TitlePage } from '@/ui/TitlePage';
+import { getData } from '@/utils/getData';
+
+export default async function Page() {
+    const { toros }: ResponseToros =
+        await getData('todosToro');
+    return (
+        <>
+            <TitlePage title="Toros" />
+            <TableBull toros={toros} />
+        </>
+    );
+}
