b58ab05a7501291966b15c2a1517542e64ec3177
Freddy Solarte <freddyjsolarte@gmail.com>
2024-05-05 10:26:44 -0400

    create services to backup & restore Bd

---
+++ src/services/backupBd.ts
@@ -0,0 +1,41 @@
+'use server';
+
+import { ResponseError } from '@/types';
+import { auth } from '@/auth';
+import { Session } from 'next-auth';
+
+export const GetBackup = async () => {
+    const session = (await auth()) as Session;
+
+    const { user } = session;
+
+    /*  const {token,cookieCsrf}=user */
+    const { token } = user;
+
+    const url = 'http://127.0.0.1:8000/api/respaldo';
+
+
+    const headers = new Headers({
+        Accept: '*/*',
+        Origin: process.env.ORIGIN,
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+    });
+
+    const optionFetch: RequestInit = {
+        cache: 'no-store',
+        method: 'GET',
+        headers: headers,
+        credentials: 'include',
+    };
+    try {
+        const res = await fetch(url, optionFetch);
+        const zip = await res.blob();
+        if (res.status == 200) return zip;
+        else throw { status: res.status, data: await res.json() };
+    } catch (e) {
+        if (e instanceof Error) throw e;
+        const { status, data } = e as ResponseError;
+        throw `código ${status} ${data.message}`;
+    }
+};
