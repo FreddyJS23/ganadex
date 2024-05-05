a75ac41bee7d2c3ee07afd485d98a964ec8523cc
Freddy Solarte <freddyjsolarte@gmail.com>
2024-05-05 10:27:30 -0400

    create action to restore BD

---
+++ src/actions/restoreBd.ts
@@ -0,0 +1,13 @@
+'use serve';
+
+import { RestoreLastBackup } from '@/services/restoreBd';
+import { ResponseError} from '@/types';
+
+export async function restoreBd(): Promise<void  | ResponseError | undefined> {
+    try {
+        await RestoreLastBackup();
+    } catch (error) {
+        const { message } = error as Error;
+        throw message;
+    }
+}
