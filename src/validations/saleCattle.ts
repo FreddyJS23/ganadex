3fa421068dc8ea4b1ee89bd9c5262ccea578542f
Freddy Solarte <freddyjsolarte@gmail.com>
2024-05-08 00:24:35 -0400

    delete validation ganado_id form create sale cattle

--- src/validations/saleCattle.ts
+++ src/validations/saleCattle.ts
@@ -2,6 +2,5 @@ import { z } from './configInitZod';
 
 export const createSaleCattleShema = z.object({
     precio: z.number(),
-    ganado_id: z.string().regex(/\d/),
     comprador_id: z.string().regex(/\d/),
 });
