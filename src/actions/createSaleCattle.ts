f987f2890ac435e5ba59e2127f11804746012fab
Freddy Solarte <freddyjsolarte@gmail.com>
2024-05-08 00:24:03 -0400

    create action to create sale cattle

---
+++ src/actions/createSaleCattle.ts
@@ -0,0 +1,23 @@
+'use serve';
+
+import { ResponseError, ResponseVentaGanado,} from '@/types';
+import { CreateSaleCattle,} from '@/types/forms';
+import { getData } from '@/utils/getData';
+
+export async function createSaleCattle(
+    formData: CreateSaleCattle,
+    id:number
+): Promise<number | ResponseError | undefined> {
+    try {
+        const  {venta} : ResponseVentaGanado = await getData(
+            'ventaGanado',
+            'POST',
+            Object.assign(formData,{ganado_id:id}),
+        );
+     
+        return venta.ganado.numero!;
+    } catch (error) {
+        const { message } = error as Error;
+        throw message;
+    }
+}
