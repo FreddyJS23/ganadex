cb2b63c8c2c427bd05f8af614bb3bc1c8fa7f03a
Freddy Solarte <freddyjsolarte@gmail.com>
2024-05-21 17:40:49 -0400

    relocation register sale cattle

--- src/app/(routes auth)/(dashboards)/venta_ganado/[id]/page.tsx
+++ src/app/(routes auth)/(dashboards)/venta_ganado/[id]/page.tsx
@@ -1,12 +1,11 @@
 import { CreateSaleCattle } from '@/components/create item in modal/create sale cattle';
-import { ResponseCompradores } from '@/types';
+import { ResponseCompradores} from '@/types';
 import { getData } from '@/utils/getData';
 
 export default async function Page() {
     const { compradores }: ResponseCompradores = await getData(
         'compradores',
     );
-    
 
     return <CreateSaleCattle ListaCompradoresRegistrados={compradores} />;
 }
