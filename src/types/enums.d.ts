6f4f0318187c61ff3db45911edd01c630682e3f4
Freddy Solarte <freddyjsolarte@gmail.com>
2024-05-30 10:22:50 -0400

    delete inputs innecesarys

--- src/types/enums.d.ts
+++ src/types/enums.d.ts
@@ -169,7 +169,6 @@ export enum FieldsIdCasttle {
     nombre,
     numero,
     origen,
-    sexo,
     tipo_id,
     fecha_nacimiento,
     peso_nacimiento,
@@ -177,14 +176,11 @@ export enum FieldsIdCasttle {
     peso_2year,
     peso_actual,
     estado_id,
-    fecha_defuncion,
-    causa_defuncion,
 }
 export enum FieldsLabelsCasttle {
     Nombre,
     Numero,
     Origen,
-    Sexo,
     Tipo,
     'Fecha de nacimiento',
     'Peso de nacimiento',
@@ -192,8 +188,6 @@ export enum FieldsLabelsCasttle {
     'Peso de 2 años',
     'Peso actual',
     Estados,
-    'Fecha de defunción',
-    'Causa de defunción',
 }
 export enum FieldsIdBull {
     nombre,
