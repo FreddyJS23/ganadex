f82164cc6f8c80dacf32ca4a0c886ee5285f605b
Freddy Solarte <freddyjsolarte@gmail.com>
2024-05-30 11:28:25 -0400

    add form register cattle

--- src/app/(routes auth)/ganado/registrar/page.tsx
+++ src/app/(routes auth)/ganado/registrar/page.tsx
@@ -1,9 +1,14 @@
-'use client';
+import { FormCow } from '@/components/forms/create cow';
+import { ResponseCompradores } from '@/types';
 import { TitlePage } from '@/ui/TitlePage';
+import { getData } from '@/utils/getData';
 
-export default function Page() {
+export default async function Page() {
+ const { compradores }: ResponseCompradores = await getData('compradores');
     return (
         <>
+            <TitlePage title="Registrar vaca" />
+            <FormCow compradores={compradores} />
         </>
     );
 }
