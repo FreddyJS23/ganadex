660f2b49f472386b99f3ab0960930a3e66793214
Freddy Solarte <freddyjsolarte@gmail.com>
2024-06-11 22:24:36 -0400

    create chart doughnut cause death cattle

---
+++ src/components/charts/dashboard/common cause dead doughnut/index.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { CausaFrecuente} from '@/types/dashboard';
+import {
+    optionChartTotalTypesCattle,
+    paletteBackground,
+} from '@/utils/configCharts';
+import { getCommonCausesDeadAndTotalDead } from '@/utils/convertResponseCommonCauseDead';
+import {
+    Chart as ChartJS,
+    ArcElement,
+    Tooltip,
+    Legend,
+    ChartData,
+} from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const TortaCausasFallecimientosGanado = ({causas_frecuentes}:{causas_frecuentes:CausaFrecuente[]}) => {
+    const { causeDead,totalDead} = getCommonCausesDeadAndTotalDead(causas_frecuentes);
+
+    const datasets: ChartData<'doughnut', number[]> = {
+        labels: causeDead,
+        datasets: [
+            {
+                label: 'Total',
+                data: totalDead,
+                backgroundColor: paletteBackground,
+            },
+        ],
+    };
+
+    return <Doughnut data={datasets} options={optionChartTotalTypesCattle} />;
+};
