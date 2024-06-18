7ac6eb9c9ee8225e930622d3296f9435526c57e6
fadrian06 <franyeradriansanchez@gmail.com>
2024-06-17 20:35:32 -0400

    style(app/error.tsx): improve readability

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,13 +2,14 @@
 
 import { useEffect } from 'react';
 
-export default function Error({
-    error,
-    reset,
-}: {
+interface Props {
     error: Error & { digest?: string };
+
+    /** To recover by trying to re-render the segment */
     reset: () => void;
-}) {
+}
+
+export default function Error({ error, reset }: Props) {
     useEffect(() => {
         // Log the error to an error reporting service
         console.error(error);
@@ -17,14 +18,7 @@ export default function Error({
     return (
         <div>
             <h2>Something went wrong!</h2>
-            <button
-                onClick={
-                    // Attempt to recover by trying to re-render the segment
-                    () => reset()
-                }
-            >
-                Try again
-            </button>
+            <button onClick={reset}>Try again</button>
         </div>
     );
 }
