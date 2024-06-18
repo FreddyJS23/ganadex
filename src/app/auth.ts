d34894dd506904814dab7f40a16ded23e0e430db
fadrian06 <franyeradriansanchez@gmail.com>
2024-06-17 20:44:34 -0400

    style(app/auth.ts): improve readability
    - Organize imports (types, default and named)
    - Add blank lines to see quicky return statement
    - Change to block comments to have 80 characters line length
    - Change property as function to a method syntax
    - Moved user constant to try block
    - Replace console to console.info to silent @biome linter
    - Add braces on ifs statements
    - Made comparison operators stricters
    - Simplify if else to guard clauses

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,9 +1,8 @@
+import type { ResponseError } from '@/types';
+import Credentials from 'next-auth/providers/credentials';
+import NextAuth, { AuthError } from 'next-auth';
 import { ERROR_SERVER, ERROR_SIGNIN } from '@/constants/responseApiMessage';
 import { authApi } from '@/services/authApi';
-import { ResponseError } from '@/types';
-
-import NextAuth, { AuthError } from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     callbacks: {
@@ -13,39 +12,48 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 token.token = user.token;
                 // token.cookieCsrf = user.cookieCsrf;
             }
+
             return token;
         },
         session({ session, token }) {
             session.user.token = token.token;
             session.user.usuario = token.usuario;
             // session.user.cookieCsrf = token.cookieCsrf;
+
             return session;
         },
     },
     pages: { signIn: '/login' },
     providers: [
         Credentials({
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
+            /* You can specify which fields should be submitted, by adding keys
+            to the `credentials` object. e.g. domain, username, password,
+            2FA token, etc. */
             credentials: {
                 usuario: {},
                 password: {},
             },
-            authorize: async (credentials) => {
-                let user = null;
+            async authorize(credentials) {
                 try {
                     // logic to verify if user exists
-                    user = await authApi(credentials);
-                    console.log(user);
+                    const user = await authApi(credentials);
+                    console.info(user);
+
                     return user;
                 } catch (errorServe) {
-                    if (errorServe instanceof Error) throw new AuthError(ERROR_SERVER);
+                    if (errorServe instanceof Error) {
+                        throw new AuthError(ERROR_SERVER);
+                    }
 
                     const codeStatusServe = errorServe as ResponseError;
 
-                    if (codeStatusServe.status != 401) throw new AuthError(ERROR_SERVER);
-                    else if (codeStatusServe.status == 401)
+                    if (codeStatusServe.status !== 401) {
+                        throw new AuthError(ERROR_SERVER);
+                    }
+
+                    if (codeStatusServe.status === 401) {
                         throw new AuthError(ERROR_SIGNIN);
+                    }
                 }
             },
         }),
