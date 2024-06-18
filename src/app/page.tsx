c663753eaa0ba3eef65dbaf9af3ea3787c32feed
fadrian06 <franyeradriansanchez@gmail.com>
2024-06-17 20:28:34 -0400

    chore(app/page.tsx): remove unreachable code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function Page() {
-   
-  redirect('/dashboard')
-  return (
-        <>
-            <div>Hola</div>
-        </>
-    );
+    redirect('/dashboard');
 }
