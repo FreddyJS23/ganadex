30d175024ba1ba59a565ec6e370d5bb5e7b9c1b9
Freddy Solarte <freddyjsolarte@gmail.com>
2024-06-25 11:00:43 -0400

    feat: add logic validation to weights cattle

--- src/validations/castleShema.ts
+++ src/validations/castleShema.ts
@@ -17,4 +17,21 @@ export const castleShema = z.object({
     precio: z.number().optional(),
     comprador_id: z.string().optional(),
     fecha_venta: z.string().optional(),
-});
+    })
+    .refine(({peso_nacimiento,peso_destete}) =>peso_destete  && peso_nacimiento &&  peso_nacimiento  <= peso_destete,
+    {message:'El peso nacimiento no puede ser mayor que el peso destete',
+    path:['peso_nacimiento']})
+    .refine(({peso_nacimiento,peso_2year}) =>peso_2year  && peso_nacimiento &&  peso_nacimiento  <= peso_2year,
+    {message:'El peso nacimiento no puede ser mayor que el peso de dos años',
+    path:['peso_nacimiento']})
+    .refine(({peso_nacimiento,peso_actual}) =>peso_actual  && peso_nacimiento &&  peso_nacimiento  <= peso_actual,
+    {message:'El peso nacimiento no puede ser mayor que el peso actual',
+    path:['peso_nacimiento']})
+    .refine(({peso_destete,peso_2year}) =>peso_destete  && peso_2year &&  peso_destete  < peso_2year,{message:'El peso destete no puede ser mayor que el peso de dos año',path:['peso_destete']})
+    .refine(({peso_destete,peso_actual}) =>peso_destete  && peso_actual &&  peso_destete  < peso_actual,{message:'El peso destete no puede ser mayor que el peso actual',path:['peso_destete']})
+    .refine(({peso_2year,peso_actual}) =>peso_2year  && peso_actual &&  peso_2year  < peso_actual,{message:'El peso de dos años no puede ser mayor que el peso actual',path:['peso_2year']})
+    .refine(({peso_destete,peso_nacimiento,peso_2year,peso_actual}) =>peso_2year  && peso_actual &&  peso_nacimiento && peso_destete &&
+     peso_actual > peso_nacimiento &&
+     peso_actual > peso_destete && 
+     peso_actual > peso_2year,
+     {message:'El peso actual no puede ser menor a los anteriores pesos',path:['peso_actual']})
