8a62bc47793f56387e740b6ef14fbeb3c0d14575
Freddy Solarte <freddyjsolarte@gmail.com>
2024-07-12 22:10:52 -0400

    improve button generate report

--- src/components/buttonPrintReports/index.tsx
+++ src/components/buttonPrintReports/index.tsx
@@ -2,18 +2,20 @@
 
 import IconImprimir from '@/icons/icono-imprimir.svg';
 import { toast } from 'sonner';
-import { endpointsReports } from '@/collections/endPointsApi';
+import Link from 'next/link';
+import { useParams, usePathname } from 'next/navigation';
 
-type ButtonGenerateReportProps = {
-    report: keyof typeof endpointsReports;
+type GenerateReport = {
+    endPoint: 'ganado' | 'dashboard';
     id?: number;
 };
 
-export const ButtonGenerateReport = ({
-    report,
-    id,
-}: ButtonGenerateReportProps) => {
-    const generateReport = async (endPoint: keyof typeof endpointsReports) => {
+export const ButtonGenerateReport = () => {
+
+    const {id} = useParams<{ id: string }>() ;
+    const pathname = usePathname();
+
+    const generateReport = async (endPoint:GenerateReport['endPoint'], id?: number) => {
         try {
             const getFile = await fetch(
                 `/api/reportes/${endPoint}${id ? `?id=${id}` : ''}  `,
@@ -30,9 +32,36 @@ export const ButtonGenerateReport = ({
         }
     };
 
-    return (
-        <button onClick={() => generateReport(report)}>
-            <IconImprimir className={'size-8'} />
-        </button>
-    );
+    const Button=({endPoint,id}:GenerateReport)=>{
+        return (
+           <button onClick={() => generateReport(endPoint,id)}>
+                <IconImprimir className={'size-8'} />
+            </button>
+    )}
+
+    /* reportes directos sin necesidad de un modal previo */
+    if (pathname == `/ganado/${id}`) return <Button endPoint={'ganado'} id={parseInt(id)} />;
+   
+    else if (pathname == '/dashboard') return <Button endPoint={'dashboard'} />;
+   
+   /*reportes con la necesidad de un modal previo */
+    else if (pathname == '/venta_leche')
+        return (
+            <Link href={`/reporte/venta_leche`}>
+                <IconImprimir className={'size-8'} />
+            </Link>
+        );
+    else if (pathname == '/fallecimientos')
+        return (
+            <Link href={`/reporte/causas_fallecimientos`}>
+                <IconImprimir className={'size-8'} />
+            </Link>
+        );
+    else if (pathname == '/venta_ganado')
+        return (
+            <Link href={`/reporte_anual/venta_ganado`}>
+                <IconImprimir className={'size-8'} />
+            </Link>
+        );
+ return ( <></>)
 };
