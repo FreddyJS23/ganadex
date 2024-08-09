03d93abb5f69c9156250ed9d5dee1c597a355f3f
Freddy Solarte <freddyjsolarte@gmail.com>
2024-08-09 17:25:18 -0400

    create page for get pajuela toro

---
+++ src/app/(routes auth)/pajuela_toro/page.tsx
@@ -0,0 +1,17 @@
+import { TablePajuelaToro } from '@/components/tables/pajuela toro';
+import {  ResponsePajuelaToros, } from '@/types';
+import { TitlePage } from '@/ui/TitlePage';
+import { getData } from '@/utils/getData';
+
+export default async function Page() {
+    const { pajuela_toros }: ResponsePajuelaToros = await getData('pajuelaToro');
+    return (
+        <>
+            <TitlePage title="Pajuela toros" />
+            <section className='max-w-44 mx-auto'>
+
+            <TablePajuelaToro pajuela_toros={pajuela_toros} />
+            </section>
+        </>
+    );
+}
