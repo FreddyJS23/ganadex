4dba55a8c3d1d87277fd2befdd74cb9f6e4bb8d2
Freddy Solarte <freddyjsolarte@gmail.com>
2024-08-19 22:54:28 -0400

    improve error for error 401 unauthenticated

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,8 @@
 'use client'; // Error components must be Client Components
 
-import { useEffect } from 'react';
+import { deleteSession } from '@/actions/deleteSession';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Error({
     error,
@@ -9,22 +11,26 @@ export default function Error({
     error: Error & { digest?: string };
     reset: () => void;
 }) {
+    const [codeError, setCodeError] = useState(0);
+
     useEffect(() => {
         // Log the error to an error reporting service
-        console.error(error);
+        const codeStatusError = error.message.split(
+            ' ',
+        )[2] as unknown as number;
+        deleteSession();
+        setCodeError(codeStatusError);
     }, [error]);
 
     return (
         <div>
-            <h2>Something went wrong!</h2>
-            <button
-                onClick={
-                    // Attempt to recover by trying to re-render the segment
-                    () => reset()
-                }
-            >
-                Try again
-            </button>
+            {codeError == 401 ? (
+                <h2>Sesión expirada, por favor vuelva a iniciar sesión</h2>
+            ) : (
+                <h2>Error inesperado</h2>
+            )}
+            <p>Error {codeError}, {codeError == 401 && 'redireccionando...'}</p>
+            {codeError != 401 && <Link href="/">Volver a inicio</Link>}
         </div>
     );
 }
