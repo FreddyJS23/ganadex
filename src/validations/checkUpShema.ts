2ff939747ce2369c0b0e45d7d5a2b3c05ec3e9d1
Freddy Solarte <freddyjsolarte@gmail.com>
2024-12-11 22:17:43 -0400

    add valiadtion to input date

--- src/validations/checkUpShema.ts
+++ src/validations/checkUpShema.ts
@@ -1,7 +1,14 @@
 import { z } from './configInitZod';
 
+const regexDate =
+    /[1-9][0-9][0-9]{2}-([0][1-9]|[1][0-2])-([1-2][0-9]|[0][1-9]|[3][0-1])/;
+
 export const createCheckUpShema = z.object({
     diagnostico: z.string().min(3).max(255),
     tratamiento: z.string().min(3).max(255),
+    fecha: z.string().regex(regexDate),
     personal_id: z.string().regex(/\d/),
-});
+}).refine((data) => new Date(data.fecha) <= new Date(), {
+    message: 'La fecha no puede ser mas alta que la fecha actual',
+    path: ['fecha'],
+});;
