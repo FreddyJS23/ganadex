f01e3b4654ea5d6f2fd163a7a52c40077a929560
Freddy Solarte <freddyjsolarte@gmail.com>
2025-01-12 21:44:48 -0400

    add list vaccination in action save form cow

--- src/actions/createCastle.ts
+++ src/actions/createCastle.ts
@@ -1,18 +1,23 @@
 'use serve';
 
-import { ResponseError, ResponseGanado } from '@/types';
+import { ListaVacunas, ResponseError, ResponseGanado } from '@/types';
 import { CreateCastle } from '@/types/forms';
 import { getData } from '@/utils/getData';
 
 export async function createCastle(
     formData: CreateCastle,
+    listVaccines: ListaVacunas[],
 ): Promise<string | number | ResponseError | undefined> {
     try {
-        const { ganado }: ResponseGanado = await getData(
+      //en esta destructuracion se saca el id y se utiliza el resto del objecto
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const vacunas=listVaccines.map(({id,...rest})=>({...rest}))
+    
+    const { ganado }: ResponseGanado = await getData(
             'ganado',
             'POST',
-            formData,
-        );
+        {...formData,vacunas},
+        );  
         if (ganado.numero) return ganado.numero;
         else if (ganado.nombre) return ganado.nombre;
     } catch (error) {
