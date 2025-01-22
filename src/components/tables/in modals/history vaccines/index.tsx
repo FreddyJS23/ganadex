e1aeab9b91c5b868a761bcd1c7b7cc66947b1699
Freddy Solarte <freddyjsolarte@gmail.com>
2025-01-22 17:27:49 -0400

    create tabble history vaccines

---
+++ src/components/tables/in modals/history vaccines/index.tsx
@@ -0,0 +1,29 @@
+import { headerHistoryVaccines } from '@/collections/headerColums';
+import { Vacuna } from '@/types';
+
+export const TableHistoryVaccines = ({
+    vacunas
+}: {vacunas:Vacuna[]}) => {
+    return (
+        <div className="overflow-x-auto">
+            <table className="table">
+                <thead>
+                    <tr>
+                        {headerHistoryVaccines.map(({ label }) => (
+                            <th key={label}>{label}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {vacunas.map(({ id, fecha, vacuna, prox_dosis }) => (
+                        <tr key={id}>
+                            <td>{vacuna}</td>
+                            <td>{fecha}</td>
+                            <td>{prox_dosis}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
