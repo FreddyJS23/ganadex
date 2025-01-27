bda16c697562fbd243cb4fff4911e0540ade29df
Freddy Solarte <freddyjsolarte@gmail.com>
2025-01-27 00:38:11 -0400

    create validation finca

---
+++ src/validations/finca.ts
@@ -0,0 +1,9 @@
+import { z } from './configInitZod';
+
+export const createFincaShema = z.object({
+    nombre: z
+        .string()
+        .regex(/^[a-zA-ZÀ-ÿ\u00f1\u00d1/\sa-zA-ZÀ-ÿ\u00f1\u00d1]{3,30}$/)
+        .min(3)
+        .max(30),
+});
