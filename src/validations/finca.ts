3572a7a4128a7c521f2302e27f5d8034c9b70b07
Freddy Solarte <freddyjsolarte@gmail.com>
2025-01-28 19:55:07 -0400

    add message error in input name finca

--- src/validations/finca.ts
+++ src/validations/finca.ts
@@ -3,7 +3,7 @@ import { z } from './configInitZod';
 export const createFincaShema = z.object({
     nombre: z
         .string()
-        .regex(/^[a-zA-ZÀ-ÿ\u00f1\u00d1/\sa-zA-ZÀ-ÿ\u00f1\u00d1]{3,30}$/)
+        .regex(/^[a-zA-ZÀ-ÿ\u00f1\u00d1/\sa-zA-ZÀ-ÿ\u00f1\u00d1]{3,30}$/,'Solo se permiten letras')
         .min(3)
         .max(30),
 });
