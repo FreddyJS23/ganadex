f220f18e5e4971626b772ed313a88226da0af805
Freddy Solarte <freddyjsolarte@gmail.com>
2025-01-28 19:49:54 -0400

    improve action finca

--- src/actions/finca.ts
+++ src/actions/finca.ts
@@ -1,6 +1,6 @@
 'use serve';
 
-import { ResponseError } from '@/types';
+import { ResponseError, ResponseFinca } from '@/types';
 import {  CreateFinca } from '@/types/forms';
 import { getData } from '@/utils/getData';
 
@@ -8,8 +8,8 @@ export async function createFinca(
     formData: CreateFinca,
 ): Promise<string | number | ResponseError | undefined> {
     try {
-        const { comprador }: Response = await getData(
-            '',
+        const { finca }: ResponseFinca = await getData(
+            'finca',
             'POST',
             formData,
         );
@@ -19,3 +19,21 @@ export async function createFinca(
         throw message;
     }
 }
+
+export async function createSesionFinca(
+    fincaId: number,
+): Promise<string | number | ResponseError | undefined> {
+    try {
+        const { finca }: ResponseFinca = await getData(
+            'crearSesionFinca',
+            'GET',
+            undefined,
+            fincaId,
+        );
+        if (finca) return finca.nombre;
+    } catch (error) {
+        const { message } = error as Error;
+        throw message;
+    }
+}
+
