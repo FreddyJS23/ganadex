e10b74fb0b9075847f6f64d73d9d260475c2aa05
Freddy Solarte <freddyjsolarte@gmail.com>
2025-01-28 20:32:44 -0400

    fix: change bad use function redirect NextResponse

--- src/app/api/signOut/route.ts
+++ src/app/api/signOut/route.ts
@@ -2,12 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { signOut as signOutAuthJs } from '@/auth';
 import { signOutApi } from '@/services/signOutApi';
+import { redirect } from 'next/navigation';
 
-export async function GET(req: NextRequest): Promise<NextResponse> {
+export async function GET(): Promise<NextResponse> {
     try {
         await signOutApi();
         return await signOutAuthJs({ redirectTo: '/login' });
     } catch (error) {
-        return NextResponse.redirect(new URL('/login', req.url));
+        return redirect('/login');
     }
 }
