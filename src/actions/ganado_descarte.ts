ecb58aa776c03205f9223178845ecc57be6feacf
Freddy Solarte <freddyjsolarte@gmail.com>
2025-02-04 22:35:42 -0400

    action: add update weight castle,bull,beef

--- src/actions/ganado_descarte.ts
+++ src/actions/ganado_descarte.ts
@@ -1,7 +1,7 @@
 'use serve';
 
-import { ResponseError, ResponseGanadoDescarte } from '@/types';
-import { CreateBeef } from '@/types/forms';
+import { Pesos, ResponseError, ResponseGanadoDescarte } from '@/types';
+import { CreateBeef, updateWeight } from '@/types/forms';
 import { getData } from '@/utils/getData';
 
 export async function createBeef(
@@ -20,3 +20,22 @@ export async function createBeef(
         throw message;
     }
 }
+
+export async function updateWeightBeef(
+    id:number,
+    formData:updateWeight,
+): Promise<Pesos| ResponseError | undefined>{
+        try {
+        const { ganado_descarte }: ResponseGanadoDescarte = await getData(
+                'ganadoDescarte',
+                'PUT',
+            formData,
+                id
+            );  
+            return ganado_descarte.pesos;
+        } catch (error) {
+            const { message } = error as Error;
+            throw message;
+        }
+}
+
