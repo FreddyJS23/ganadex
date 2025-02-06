e648e68968533d5e5cbd5b31192ddb81b2c707ef
Freddy Solarte <freddyjsolarte@gmail.com>
2025-02-06 19:28:41 -0400

    improve table user veterinary

--- src/components/tables/veterinary users/index.tsx
+++ src/components/tables/veterinary users/index.tsx
@@ -10,55 +10,65 @@ import {
     TableCell,
     Table,
 } from '@nextui-org/table';
-import { Key, ReactNode, useCallback } from 'react';
-import { LayoutTable } from '..';
-
-const data: UserVeterinaryInfo[] = [
-    {
-        id: 1,
-        usuario: 'pedro',
-        email: 'pedro@gmail.com',
-        rol: 'veterinario',
-    },
-];
+import { Key, ReactNode, useCallback, useState } from 'react';
+import { LayoutTable, TableComponent } from '..';
+import { useDisclosure } from '@nextui-org/react';
+import IconDelete from '@/icons/icono-error.svg';
+import { ModalDeleteUserVeterinary } from '@/components/modals/delete user veterinary';
 
 export const TableVeterinaryUsers = ({
-    usuarios_veterinarios = data,
+    usuarios_veterinarios,
 }: ResponseVeterinariosUsuario) => {
+   
+    /* Estado modal */
+    const { isOpen, onOpen,onOpenChange,onClose } = useDisclosure();
+    /* data modal */
+    const [dataModal, setDataModal] = useState({id:0,usuario:''});
+    
+    const handleOpen=(id:number,usuario:string)=>{
+        setDataModal({id,usuario});
+        onOpen();
+    }
+   
     const renderCell = useCallback(
         (usuario_veterinario: UserVeterinaryInfo, columnKey: Key) => {
             const cellValue =
                 usuario_veterinario[columnKey as keyof UserVeterinaryInfo];
-            return cellValue as ReactNode;
+                switch (columnKey as keyof UserVeterinaryInfo) {
+                    /* button icon */
+                    case 'id': {
+                        const id = cellValue as number;
+                        return (
+                        <IconDelete 
+                        onClick={()=>handleOpen(id,usuario_veterinario['usuario'])} 
+                        className={'size-6 cursor-pointer'} />
+                            
+                        );
+                    }
+    
+                    default:
+                        return cellValue as ReactNode;
+                }
         },
         [],
     );
 
     return (
-        <Table
-            removeWrapper
-            aria-label={`TableuserVeterinary`}
-            classNames={{
-                wrapper: 'bg-base-100',
-                th: 'bg-base-200 font-bold text-current',
-            }}
-        >
-            <TableHeader columns={headersColumnsUserVeterinary}>
-                {({ key, label }) => (
-                    <TableColumn key={key}>{label}</TableColumn>
-                )}
-            </TableHeader>
-            <TableBody items={usuarios_veterinarios}>
-                {(usuario_veterinario) => (
-                    <TableRow key={usuario_veterinario.id}>
-                        {(columnKey) => (
-                            <TableCell>
-                                {renderCell(usuario_veterinario, columnKey)}
-                            </TableCell>
-                        )}
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
+        <>
+        <TableComponent
+        type="userVeterinarys"
+        columnsCollection={headersColumnsUserVeterinary}
+        items={usuarios_veterinarios}
+        renderCell={renderCell}
+    />
+    <ModalDeleteUserVeterinary
+    id={dataModal.id}
+    isOpen={isOpen}
+    onOpenChange={onOpenChange}
+    dataHeader={dataModal.usuario}
+    onClose={onClose}
+    
+    />
+        </>
     );
 };
