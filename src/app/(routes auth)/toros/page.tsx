3840290105d341dee24cc1d183ea0b8978eab3f6
Freddy Solarte <freddyjsolarte@gmail.com>
2025-02-20 19:12:30 -0400

    Update page.tsx

--- src/app/(routes auth)/toros/page.tsx
+++ src/app/(routes auth)/toros/page.tsx
@@ -1,14 +1,18 @@
+import { auth } from '@/app/auth';
 import { TableBull } from '@/components/tables/bull';
 import { ResponseToros } from '@/types';
 import { TitlePage } from '@/ui/TitlePage';
 import { getData } from '@/utils/getData';
+import { Session } from 'next-auth';
 
 export default async function Page() {
     const { toros }: ResponseToros = await getData('todosToro');
+    const session = await auth() as Session
+    const role=session.user.rol
     return (
         <>
             <TitlePage title="Toros" />
-            <TableBull toros={toros} />
+            <TableBull toros={toros} role={role} />
         </>
     );
 }
