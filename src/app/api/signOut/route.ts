a65360b2e1beee69ef4003e15ea2d8a0c4ed7595
Freddy Solarte <freddyjsolarte@gmail.com>
2025-02-21 01:29:45 -0400

    fix: signtOut

--- src/app/api/signOut/route.ts
+++ src/app/api/signOut/route.ts
@@ -5,11 +5,9 @@ import { signOutApi } from '@/services/signOutApi';
 import { redirect } from 'next/navigation';
 
 export async function GET(): Promise<NextResponse> {
-    try {
+    
         await signOutApi();
         await signOutAuthJs({ 'redirect':false });
+        
         return redirect('/login')
-    } catch (error) {
-        return redirect('/login');
-    }
 }
