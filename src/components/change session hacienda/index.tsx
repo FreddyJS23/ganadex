6de204ace959479e8db7f6092f03a8a003818bd7
Freddy Solarte <freddyjsolarte@gmail.com>
2025-03-18 09:41:13 -0400

    create component to change session

---
+++ src/components/change session hacienda/index.tsx
@@ -0,0 +1,124 @@
+import { changeSessionHacienda } from '@/actions/cambiarSesionHacienda';
+import { Hacienda } from '@/types';
+import { messageErrorApi } from '@/utils/handleErrorResponseNext';
+import {
+    Button as ButtonNextUI,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    useDisclosure,
+} from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { toast } from 'sonner';
+import { Button } from '@/ui/Button';
+import IconHacienda from '@/icons/icono-hacienda.svg';
+import IconArrowRight from '@/icons/icono-flecha_derecha1.svg';
+
+type ModalProps = {
+    isOpen: boolean;
+    onOpenChange: (isOpen: boolean) => void;
+    hacienda: Hacienda;
+};
+
+export const ChangeSessionHacienda = ({ hacienda }: { hacienda: Hacienda }) => {
+    /* Estado modal */
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+    return (
+        <>
+     
+            <ButtonNextUI size='sm' color={'primary'} className='w-28 mt-1 ' onClick={onOpen} >
+                Trabajar
+            </ButtonNextUI>
+
+      
+            <ModalHaciendaSession isOpen={isOpen} onOpenChange={onOpenChange} hacienda={hacienda} />
+        </>
+    );
+};
+
+const ModalHaciendaSession = ({
+    isOpen,
+    onOpenChange,
+    hacienda,
+}: ModalProps) => {
+    const router = useRouter();
+
+    const actionChangeSessionHacienda = async (hacienda_id: number) => {
+        const response = await changeSessionHacienda(hacienda_id);
+
+        /*ver si es tipo objecto para evitar conflictos de tipo para manejar error del backedn y mostar mensaje */
+        if (typeof response == 'object')
+            if ('error' in response)
+                return toast.error(messageErrorApi(response));
+
+        toast.success(`Empezando a trabajar en ${response.hacienda.nombre}`);
+        router.push(`/api/verificar_sesion_hacienda`);
+    };
+
+    return (
+        <Modal
+            classNames={{ base: 'bg-base-100' }}
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+        >
+            <ModalContent>
+                {(onClose) => (
+                    <>
+                        <ModalHeader className="flex flex-col gap-1">
+                            <h3>Cambiar área de trabajo</h3>
+                            <p className="text-sm opacity-60">
+                                Estás a punto de cambiar la sesión de trabajo
+                                con esta hacienda
+                            </p>
+                        </ModalHeader>
+                        <ModalBody>
+                            {/* detalles de la hacienda */}
+                            <div className="flex flex-col gap-4 py-4">
+                                <div className="flex items-center gap-4">
+                                    <div className="w-16 h-16  overflow-hidden">
+                                        <IconHacienda className='w-full h-full object-cover' />
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <h2 className="text-lg font-bold">
+                                            {hacienda.nombre}
+                                        </h2>
+                                        <span className="text-sm opacity-60">
+                                            {hacienda.fecha_creacion}
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div className="divider divider-horizontal divider-primary opacity-20"></div>
+
+                                <div className="text-sm opacity-60">
+                                    Al cambiar la sesión por esta hacienda, podrás
+                                    acceder a toda su información, gestionar sus
+                                    recursos y realizar operaciones.
+                                </div>
+                            </div>
+                        </ModalBody>
+                        <ModalFooter>
+                        <Button
+                                onClick={onClose}
+                                color="default"
+                                content="Cancelar"
+                            />
+                           
+                            <Button
+                                type="submit"
+                                onClick={() => actionChangeSessionHacienda(hacienda.id)}
+                                content={'Confirmar'}
+                                
+                            />
+                           
+                        </ModalFooter>
+                    </>
+                )}
+            </ModalContent>
+        </Modal>
+    );
+};
