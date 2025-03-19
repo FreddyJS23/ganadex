3fb62ca49eea6106f86de2c34d5aac783656b18d
Freddy Solarte <freddyjsolarte@gmail.com>
2025-03-19 14:00:29 -0400

    get session to send in component table

--- src/app/(routes auth)/personal/page.tsx
+++ src/app/(routes auth)/personal/page.tsx
@@ -1,15 +1,20 @@
+import { auth } from '@/app/auth';
 import { TableStaff } from '@/components/tables/staff';
 import { ResponseTodoPersonal } from '@/types';
 import { TitlePage } from '@/ui/TitlePage';
 import { getData } from '@/utils/getData';
+import { Session } from 'next-auth';
 
 export default async function Page() {
     const { todo_personal }: ResponseTodoPersonal =
         await getData('todosPersonal');
-    return (
+
+        const session = await auth() as Session
+        const nameHacienda=session.user.hacienda?.nombre ?? ''
+        return (
         <section>
             <TitlePage title="Personal" />
-            <TableStaff todo_personal={todo_personal} />
+            <TableStaff todo_personal={todo_personal} nameHacienda={nameHacienda} />
         </section>
     );
 }
