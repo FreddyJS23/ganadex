cf49603f5cce531c7a2e10391b93a9b0ab64d6f8
Freddy Solarte <freddyjsolarte@gmail.com>
2025-03-20 19:20:25 -0400

    update information modal birth details

--- src/components/modals/birth/index.tsx
+++ src/components/modals/birth/index.tsx
@@ -1,11 +1,30 @@
 'use client';
 
+import { RedirectInTable } from '@/components/redirectsInTables';
 import { LayoutModal } from '..';
 import { ModalProps, Parto } from '@/types';
 
 export const ModalBirth = ({ parto }: ModalProps & { parto: Parto }) => {
-    const { cria, fecha, observacion, padre_toro,pajuela_toro, veterinario } = parto;
-    const { nombre, numero, sexo, peso } = cria;
+    const { crias, fecha, observacion, padre_toro, pajuela_toro, personal } =
+        parto;
+
+        //si hay mas de dos crias en el parto
+        const gemelos=crias.length > 1
+
+        const typeBaby=(id:number,toro_id:number | null,descarte_id:number | null,numero:number | null)=>{
+           let redirect:"ganado" | "toros" | "ganado_descarte"='ganado'
+
+           if(toro_id){
+            id=toro_id
+            redirect='toros'
+           }
+           else if(descarte_id){
+            id=descarte_id
+            redirect="ganado_descarte"
+           }
+           
+           return <RedirectInTable label={numero} id={id} redirect={redirect} />
+        }
 
     return (
         <LayoutModal
@@ -17,27 +36,52 @@ export const ModalBirth = ({ parto }: ModalProps & { parto: Parto }) => {
         >
             <div className="flex flex-col gap-4 mb-4">
                 <p>
-                    <b>Observación: </b> {observacion}
+                    <b>Observación del parto: </b> {observacion}
                 </p>
-                <p>
-                    <b>Padre de la cría: </b> {padre_toro ? padre_toro?.numero : pajuela_toro?.codigo}
+                <p className='flex gap-1'>
+                    <b>Padre de la cría: </b>{' '}
+                    {padre_toro ? <RedirectInTable id={padre_toro.id} label={padre_toro.numero} redirect='toros' /> : pajuela_toro?.codigo}
                 </p>
-                <p>
-                    <b>Nombre de la cría: </b> {nombre}
-                </p>
-                <p>
-                    <b>Numero de la cría: </b> {numero}
-                </p>
-                <p>
-                    <b>Peso nacimiento de la cría: </b> {peso.peso_nacimiento}
-                </p>
+                {crias.map(
+                    ({
+                        nombre,
+                        numero,
+                        peso_nacimiento,
+                        sexo,
+                        observacion,
+                        id,
+                        toro_id,
+                        descarte_id,
+                    },index) => {
+                        return (
+                            <>
+                                {/* al ser dos crias se coloca una numeracion a cada una */}
+                                {gemelos && <b>Cría #{index + 1}</b>}
+                                
+                                <p>
+                                    <b>Nombre de la cría: </b> {nombre}
+                                </p>
+                                <p className='flex gap-1'>
+                                    <b>Numero de la cría: </b> {typeBaby(id,toro_id,descarte_id,numero)}
+                                </p>
+                                <p>
+                                    <b>Peso nacimiento de la cría: </b>
+                                    {peso_nacimiento + ' kg'}
+                                </p>
+                                <p>
+                                    <b>Sexo de la cría: </b> {sexo}
+                                </p>
+                                <p>
+                                    <b>Observación de la cría: </b> {observacion}
+                                </p>
+                            </>
+                        );
+                    },
+                )}
 
                 <p>
-                    <b>Sexo de la cría: </b> {sexo}
-                </p>
-                <p>
-                    <b>Veterinario que atendió el parto: </b>{' '}
-                    {veterinario.nombre}
+                    <b>{personal.cargo == 'veterinario' ? 'Veterinario ' : 'Obrero '} que atendió el parto: </b>{' '}
+                    {personal.nombre}
                 </p>
             </div>
         </LayoutModal>
