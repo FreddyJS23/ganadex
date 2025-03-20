c1bbd71656f3a5785fa6cc07e2c364b157b2b97d
Freddy Solarte <freddyjsolarte@gmail.com>
2025-03-20 19:22:18 -0400

    refactor: improve href redirect tables
    la navegacion antigua se hacia sobre la ruta actual ocasinado que si no se estaba en rutas principal la url destino seria incorrecto, ahora se usa rutas absolutas

--- src/components/redirectsInTables/index.tsx
+++ src/components/redirectsInTables/index.tsx
@@ -11,7 +11,7 @@ export const RedirectInTable = ({ id, label, redirect }: RedirectInTable) => {
     return (
         <div className="flex gap-2">
             <span>{label}</span>
-            <Link href={`${redirect}/${id}`}>
+            <Link href={`/${redirect}/${id}`}>
                 <IconRedirect className="size-5" />
             </Link>
         </div>
