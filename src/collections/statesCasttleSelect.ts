86fcbe5454fc1dafec447dbc25a589b1e055f62f
Freddy Solarte <freddyjsolarte@gmail.com>
2025-04-01 09:31:50 -0400

    renove state birth in register cow
    por ahora se removio, ya que al poner un estado de gestacion se necesita de mucha mas logica, como por ejemplo crear el servicio y poner fecha de parto para esta vaca que esta en gestacion

--- src/collections/statesCasttleSelect.ts
+++ src/collections/statesCasttleSelect.ts
@@ -1,7 +1,6 @@
 export const stateCasttleSelect = [
   { value: 1, label: "Sano" },
   { value: 2, label: "Fallecido" },
-  { value: 3, label: "Gestacion" },
   { value: 4, label: "Lactancia" },
   { value: 5, label: "Vendido" },
   { value: 6, label: "Pendiente de revision" },
