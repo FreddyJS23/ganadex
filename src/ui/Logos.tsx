5351b6db4cbb5d5264a7ef1268e2da69f018bd91
Freddy Solarte <freddyjsolarte@gmail.com>
2025-04-01 15:03:09 -0400

    fix: icon big in sidebar tablet

--- src/ui/Logos.tsx
+++ src/ui/Logos.tsx
@@ -8,16 +8,16 @@ type LogosProps = {
 export const Logos = ({ small }: LogosProps) => {
   return (
     <>
-      <div className={`block dark:hidden  ${small ? "self-center" : ""} `}>
-        <IconoLogo className={small ? "size-16" : "size-36"} />
+      <div className={`block dark:hidden pt-4  ${small ? "self-center" : ""} `}>
+        <IconoLogo className={small ? "size-10" : "size-32"} />
       </div>
-      <div className={`hidden dark:block ${small ? "self-center" : ""} `}>
+      <div className={`hidden dark:block pt-4 ${small ? "self-center" : ""} `}>
         <div className="hidden sm:block">
-          <IconoLogoDark className={small ? "size-16" : "size-36"} />
+          <IconoLogoDark className={small ? "size-10" : "size-32"} />
         </div>
-        <div className="block sm:hidden">
-          <IconoLogo className={small ? "size-16" : "size-36"} />
-        </div>
+         <div className="block sm:hidden">
+          <IconoLogo className={small ? "size-10" : "size-32"} />
+        </div> 
       </div>
     </>
   );
