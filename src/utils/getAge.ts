b5c0ab097a265714797c1cd89c85830c67caad9a
Freddy Solarte <freddyjsolarte@gmail.com>
2025-05-14 00:46:31 -0400

    fix: bad format date convert

--- src/utils/getAge.ts
+++ src/utils/getAge.ts
@@ -1,6 +1,14 @@
+/** Función que devuelve la edad del animal a partir de su fecha de nacimiento
+ * @param fecha fecha de nacimiento del animal formato dd/mm/yyyy(d-m-a)
+ */
 export const getAge = (fecha: string) => {
+  
+  const fechaArray= fecha.split("-");
+//ya que el formato de fecha es dd-mm-aaaa hay que convertir a aaaa-mm-dd para el date
+  const fechaFormato = fechaArray[2] + "-" + fechaArray[1] + "-" + fechaArray[0];
+  
   const fechaActual = new Date();
-  const fechaNacimiento = new Date(fecha);
+  const fechaNacimiento = new Date(fechaFormato);
   const ageYear = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
   const ageMonth = fechaActual.getMonth() - fechaNacimiento.getMonth();
   const ageDays = fechaActual.getDate() - fechaNacimiento.getDate();
